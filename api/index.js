7eb0c599be2abaaa79e6d4a96232193d68311a56
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-04-06 11:07:15 +0100

    Delete unused route
    Fixes https://github.com/carrismetropolitana/schedules-api/security/code-scanning/11

--- api/index.js
+++ api/index.js
@@ -82,28 +82,6 @@ app.get('/api/routes/route_short_name/:route_short_name', async (req, res) => {
 });
 
 //
-app.get('/api/pdf/:stop_id/:route_short_name/:direction_id', async (req, res) => {
-  try {
-    const pdf_base_url = 'https://raw.githubusercontent.com/carrismetropolitana/pdfs/novos-horarios/horarios/';
-    const pdf_filename = `horario-singular-${req.params.stop_id}-${req.params.route_short_name}-${req.params.direction_id}.pdf`;
-    const response = await fetch(pdf_base_url + pdf_filename);
-    if (response.ok) {
-      console.log(`🟢 → Request for "/api/pdf/${req.params.stop_id}/${req.params.route_short_name}/${req.params.direction_id}": File Exists`);
-      res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader('Content-Disposition', `attachment; filename="${pdf_filename}"`);
-      const pdfStream = Readable.from(response.body);
-      pdfStream.pipe(res);
-    } else {
-      console.log(`🟡 → Request for "/api/pdf/${req.params.stop_id}/${req.params.route_short_name}/${req.params.direction_id}": File Not Found`);
-      res.status(404).send();
-    }
-  } catch (err) {
-    console.log(`🔴 → Request for "/api/pdf/${req.params.stop_id}/${req.params.route_short_name}/${req.params.direction_id}": Server Error`, err);
-    res.status(500).send({});
-  }
-});
-
-//
 app.get('/api/stops', async (req, res) => {
   try {
     const foundDocuments = await GTFSAPIDB.Stop.find({});
