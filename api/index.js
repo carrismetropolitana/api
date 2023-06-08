0329aa587b6b1ff151220ebcf5be993473488de4
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-06-08 23:09:10 +0100

    Cleanup

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const rateLimit = require('express-rate-limit');
 app.use(
   rateLimit({
     windowMs: 1 * 60 * 1000, // 1 minute
-    max: 50, // Limit each IP to 50 requests per `window` (here, per 1 minute)
+    max: 50, // Limit each IP to 50 requests per `windowMs` (here, per 1 minute)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   })
