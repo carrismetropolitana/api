f319b31e5b7c89b8fff64dad480b8dc2751ceff5
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-06-18 03:56:51 +0100

    Removed parallel processing

--- daemon/modules/updateShapes.js
+++ daemon/modules/updateShapes.js
@@ -1,9 +1,6 @@
 const GTFSParseDB = require('../databases/gtfsparsedb');
 const GTFSAPIDB = require('../databases/gtfsapidb');
 const timeCalc = require('./timeCalc');
-const splitIntoChunks = require('./splitIntoChunks');
-const Piscina = require('piscina');
-const { resolve } = require('path');
 
 module.exports = async () => {
   // Record the start time to later calculate operation duration
@@ -26,20 +23,31 @@ module.exports = async () => {
         GROUP BY
             shape_id
     `);
-  // Split the array into chunks
-  const allChunks = splitIntoChunks(allShapes.rows, 5);
-  // Initiate the worker threads for processing Shapes in parallel
-  console.log(`⤷ Setting up workers for ${allShapes.rows.length} Shapes divided into ${allChunks.length} chunks...`);
-  const piscina = new Piscina({ filename: resolve(__dirname, 'updateShapesWorker.js') });
-  // Setup a tasks for each shape and await completion for all of them
-  console.log(`⤷ Awaiting for tasks to complete...`);
-  const workerResult = await Promise.all(allChunks.map(async (chunk) => await piscina.run({ chunk })));
-  const updatedShapeIds = workerResult.flat();
-  console.log(`⤷ Updated ${updatedShapeIds.length} Shapes.`);
+  // Initiate variable to keep track of updated _ids
+  let updatedShapeIds = [];
+  // Loop through each object in each chunk
+  for (const shape of allShapes.rows) {
+    // Initiate a variable to hold the parsed shape
+    let parsedShape = {
+      code: shape.shape_id,
+    };
+    // Sort points to match sequence
+    const collator = new Intl.Collator('en', { numeric: true, sensitivity: 'base' });
+    parsedShape.points = shape.points.sort((a, b) => collator.compare(a.shape_pt_sequence, b.shape_pt_sequence));
+    // Create geojson feature using turf
+    parsedShape.geojson = turf.lineString(parsedShape.points.map((point) => [parseFloat(point.shape_pt_lon), parseFloat(point.shape_pt_lat)]));
+    // Calculate shape extension
+    const shapeExtensionKm = turf.length(parsedShape.geojson, { units: 'kilometers' });
+    parsedShape.extension = shapeExtensionKm ? shapeExtensionKm / 1000 : 0;
+    // Update or create new document
+    const updatedShapeDocument = await GTFSAPIDB.Shape.findOneAndReplace({ code: parsedShape.code }, parsedShape, { new: true, upsert: true });
+    // Return _id to main thread
+    updatedShapeIds.push(updatedShapeDocument._id);
+  }
   // Delete all Shapes not present in the current update
   const deletedStaleShapes = await GTFSAPIDB.Shape.deleteMany({ _id: { $nin: updatedShapeIds } });
   console.log(`⤷ Deleted ${deletedStaleShapes.deletedCount} stale Shapes.`);
   // Log how long it took to process everything
   const elapsedTime = timeCalc.getElapsedTime(startTime);
-  console.log(`⤷ Done updating Shapes (${elapsedTime}).`);
+  console.log(`⤷ Done updating Shapes (${updatedShapeIds.length} in ${elapsedTime}).`);
 };
