258cb0c2bfb6077d494b23c54a9eacfba7ab42e4
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-07-10 15:22:10 +0100

    Disable modules

--- daemon/index.js
+++ daemon/index.js
@@ -44,25 +44,25 @@ async function appInitPoint() {
     await GTFSParseDB.connect();
     await GTFSAPIDB.connect();
 
-    console.log();
-    console.log('STEP 2: Fetching latest GTFS archive');
-    await filemanager.downloadFromUrl(GTFS_URL);
+    // console.log();
+    // console.log('STEP 2: Fetching latest GTFS archive');
+    // await filemanager.downloadFromUrl(GTFS_URL);
 
-    console.log();
-    console.log('STEP 3: Extracting downloaded archive');
-    await filemanager.extractArchive();
+    // console.log();
+    // console.log('STEP 3: Extracting downloaded archive');
+    // await filemanager.extractArchive();
 
-    console.log();
-    console.log('STEP 4: Setup SQL tables to store the GTFS files');
-    await setupSqlTables();
+    // console.log();
+    // console.log('STEP 4: Setup SQL tables to store the GTFS files');
+    // await setupSqlTables();
 
-    console.log();
-    console.log('STEP 5: Import extracted files into created tables');
-    await saveFilesToTables();
+    // console.log();
+    // console.log('STEP 5: Import extracted files into created tables');
+    // await saveFilesToTables();
 
-    console.log();
-    console.log('STEP 6: Cleanup temp files');
-    await filemanager.removeTempDirectory();
+    // console.log();
+    // console.log('STEP 6: Cleanup temp files');
+    // await filemanager.removeTempDirectory();
 
     console.log();
     console.log('STEP 7: Update Municipalities');
