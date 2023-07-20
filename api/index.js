aa624a75296cceddb428e8050be9a080ba01e99d
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-07-20 10:55:41 +0100

    Turn off PDFs

--- api/index.js
+++ api/index.js
@@ -119,22 +119,23 @@ app.get('/stops/:stop_id', async (req, res) => {
 
 //
 app.get('/pdf/:stop_id/:route_short_name/:direction_id', async (req, res) => {
+  return await res.status(200).send();
   try {
     const pdf_base_url = 'https://raw.githubusercontent.com/carrismetropolitana/pdfs/latest/horarios/';
     const pdf_filename = `horario-singular-${req.params.stop_id}-${req.params.route_short_name}-${req.params.direction_id}.pdf`;
     const response = await fetch(pdf_base_url + pdf_filename);
     if (response.ok) {
-      res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader('Content-Disposition', `attachment; filename="${pdf_filename}"`);
+      await res.setHeader('Content-Type', 'application/pdf');
+      await res.setHeader('Content-Disposition', `attachment; filename="${pdf_filename}"`);
       const pdfStream = Readable.from(response.body);
       pdfStream.pipe(res);
       console.log(`🟢 → Request for "/api/pdf/${req.params.stop_id}/${req.params.route_short_name}/${req.params.direction_id}": File Exists`);
     } else {
-      res.status(200).send();
+      await res.status(200).send();
       console.log(`🟡 → Request for "/api/pdf/${req.params.stop_id}/${req.params.route_short_name}/${req.params.direction_id}": File Not Found`);
     }
   } catch (err) {
-    res.status(200).send();
+    await res.status(200).send();
     console.log(`🔴 → Request for "/api/pdf/${req.params.stop_id}/${req.params.route_short_name}/${req.params.direction_id}": Server Error`, err);
   }
 });
