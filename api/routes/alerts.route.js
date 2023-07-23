b5c520d3f3bb685136ee5e7211116b2d09216a42
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-07-23 21:01:58 +0100

    Used https://github.com/ricardojorgerm/alerts-gtfs to serve Alerts in GTFS-RT

--- api/routes/alerts.route.js
+++ api/routes/alerts.route.js
@@ -1,84 +1,15 @@
 /* * */
 /* IMPORTS */
 const express = require('express');
-const GTFSAPIDB = require('../services/GTFSAPIDB');
 const router = express.Router();
-const protobuf = require('protobufjs');
-const GTFSRealTimeBindings = require('../protos/gtfsrt');
+const alertsRt = require('../services/alerts-rt');
 //
 router.get('/', async (req, res) => {
   try {
     const allAlertsResponse = await fetch('https://www.carrismetropolitana.pt/?api=alerts');
     const allAlerts = await allAlertsResponse.json();
-
-    // console.log(GTFSRealTimeBindings.transit_realtime.FeedMessage);
-
-    const message = GTFSRealTimeBindings.transit_realtime.FeedMessage.create({
-      FeedHeader: GTFSRealTimeBindings.transit_realtime.FeedHeader.encode({
-        gtfsRealtimeVersion: '2.0',
-        timestamp: Date.now(),
-        incrementality: 0,
-      }),
-    });
-
-    var buffer = GTFSRealTimeBindings.transit_realtime.FeedMessage.encode(message).finish();
-
-    console.log('buffer', buffer);
-    console.log('Uint8Array(buffer)', Uint8Array(buffer));
-
-    await res.send(buffer);
-
-    return;
-
-    protobuf.load('./protos/gtfs-realtime.proto', async function (err, root) {
-      if (err) throw err;
-      // Obtain a message type
-      var FeedMessage = root.lookupType('transit_realtime.FeedMessage');
-      var FeedHeader = root.lookupType('transit_realtime.FeedHeader');
-
-      const header = FeedHeader.create({ ...allAlerts.header, gtfs_realtime_version: 2.0, incrementality: 0 });
-
-      const finalMessage = FeedMessage.create({ header: header });
-
-      var buffer = FeedMessage.encode(finalMessage).finish();
-
-      await res.send(buffer);
-    });
-
-    protobuf.load('../protos/gtfs-realtime.proto', function (err, root) {
-      if (err) throw err;
-      // Obtain a message type
-      var FeedMessage = root.lookupType('transit_realtime.FeedMessage');
-      var FeedHeader = root.lookupType('transit_realtime.FeedHeader');
-      var FeedEntity = root.lookupType('transit_realtime.FeedEntity');
-      var Alert = root.lookupType('transit_realtime.Alert');
-      var TimeRange = root.lookupType('transit_realtime.TimeRange');
-      var TranslatedString = root.lookupType('transit_realtime.TranslatedString');
-      var TranslatedImage = root.lookupType('transit_realtime.TranslatedImage');
-      var EntitySelector = root.lookupType('transit_realtime.EntitySelector');
-      for (const entity of allAlerts.entity) {
-        for (const alert of entity.alert) {
-          Alert.create({
-            active_period: TimeRange.create({
-              start: alert.active_period.start,
-              end: alert.active_period.end,
-            }),
-            informed_entity: EntitySelector.create(),
-          });
-        }
-      }
-    });
-
-    const foundManyDocuments = await GTFSAPIDB.Line.find();
-    if (foundManyDocuments.length > 0) {
-      const collator = new Intl.Collator('en', { numeric: true, sensitivity: 'base' });
-      foundManyDocuments.sort((a, b) => collator.compare(a.code, b.code));
-      console.log('🟢 → Request for "/lines/[all]": %s Found', foundManyDocuments.length);
-      await res.send(foundManyDocuments);
-    } else {
-      console.log('🟡 → Request for "/lines/[all]": Not Found');
-      await res.status(404).send([]);
-    }
+    const feedBuffer = alertsRt.createAlertFeed(allAlerts);
+    await res.send(feedBuffer);
   } catch (err) {
     console.log('🔴 → Request for "/lines/[all]": Server Error', err);
     await res.status(500).send([]);
---
+++ api/services/alerts-rt.js
@@ -0,0 +1,90 @@
+const protobuf = require('protobufjs');
+const gtfsRealtime = protobuf.loadSync('gtfs-realtime.proto');
+
+function createAlertFeed(alertJson) {
+  const FeedMessage = gtfsRealtime.root.lookupType('transit_realtime.FeedMessage');
+
+  const feed = {
+    header: {
+      gtfsRealtimeVersion: '2.0',
+      incrementality: 0,
+      timestamp: Math.floor(Date.now() / 1000),
+    },
+    entity: [],
+  };
+
+  const Alert = gtfsRealtime.root.lookupType('transit_realtime.Alert');
+  const TranslatedString = gtfsRealtime.root.lookupType('transit_realtime.TranslatedString');
+
+  for (let entity of alertJson.entity) {
+    let alert = {
+      cause: Alert.Cause[entity.alert.cause],
+      effect: Alert.Effect[entity.alert.effect],
+      url: TranslatedString.fromObject({
+        translation: [
+          TranslatedString.Translation.create({
+            text: entity.alert.pid,
+            language: 'pt',
+          }),
+        ],
+      }),
+      activePeriod: [
+        {
+          start: entity.alert.active_period.start,
+          end: entity.alert.active_period.end,
+        },
+      ],
+      headerText: TranslatedString.fromObject({
+        translation: [TranslatedString.Translation.create(entity.alert.header_text[0].translation)],
+      }),
+      descriptionText: TranslatedString.fromObject({
+        translation: [TranslatedString.Translation.create(entity.alert.description_text[0].translation)],
+      }),
+      informedEntity: [],
+    };
+    if (entity.alert.image[0].localized_image.url != '') {
+      alert.image = {
+        localizedImage: [
+          {
+            language: entity.alert.image[0].localized_image.language,
+            mediaType: entity.alert.image[0].localized_image.media_type,
+            url: entity.alert.image[0].localized_image.url,
+          },
+        ],
+      };
+    }
+    for (let informed_entity of entity.alert.informed_entity) {
+      if (informed_entity.stop_id) {
+        alert.informedEntity.push({
+          stopId: informed_entity.stop_id,
+        });
+      } else if (informed_entity.route_id) {
+        alert.informedEntity.push({
+          routeId: informed_entity.route_id,
+        });
+      }
+    }
+    feed.entity.push({
+      id: entity.id,
+      alert: alert,
+    });
+  }
+  const message = FeedMessage.create(feed);
+  const buffer = FeedMessage.encode(message).finish();
+
+  // Decode binary to JSON
+  const testDecodeMessage = FeedMessage.decode(buffer);
+  const testDecodeMessageJson = FeedMessage.toObject(testDecodeMessage);
+  // json now contains decoded data
+  console.log(testDecodeMessageJson);
+  console.log(testDecodeMessageJson.entity[1]);
+  console.log(testDecodeMessageJson.entity[1].alert);
+  console.log(testDecodeMessageJson.entity[1].alert.activePeriod[0].start);
+  console.log(testDecodeMessageJson.entity.slice(-1)[0]);
+  console.log(testDecodeMessageJson.entity.slice(-1)[0].alert);
+  return buffer;
+}
+
+module.exports = {
+  createAlertFeed,
+};
