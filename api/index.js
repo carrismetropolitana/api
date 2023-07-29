52b7fd4baf39e24d4556ae4a1b35eb35c83c1feb
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-07-29 20:08:38 +0100

    Switch to Fastify (try 3)

--- api/index.js
+++ api/index.js
@@ -1,149 +1,116 @@
 /* * */
 /* IMPORTS */
-const express = require('express');
-const app = express();
+const fastify = require('fastify')({ logger: true, requestTimeout: 20000 });
 const GTFSAPIDB = require('./databases/gtfsapidb');
-const { Readable } = require('stream');
-
-// Set CORS Header globally
-app.use(function (req, res, next) {
-  req.setTimeout(5000, () => console.log('Timeout on ', req.originalUrl));
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-});
 
 //
-app.get('/routes', async (req, res) => {
-  try {
-    const foundManyDocuments = await GTFSAPIDB.Route.find({});
-    if (foundManyDocuments.length > 0) {
-      foundManyDocuments.sort((a, b) => (a.route_id > b.route_id ? 1 : -1));
-      await res.status(200).send(foundManyDocuments);
-      console.log('🟢 → Request for "/routes/[all]": %s Found', foundManyDocuments.length);
-    } else {
-      await res.status(404).send([]);
-      console.log('🟡 → Request for "/routes/[all]": Not Found');
-    }
-  } catch (err) {
-    await res.status(500).send([]);
-    console.log('🔴 → Request for "/routes/[all]": Server Error', err);
-  }
-});
-
-//
-app.get('/routes/summary', async (req, res) => {
+fastify.get('/routes/summary', async (request, reply) => {
   try {
     const foundManyDocuments = await GTFSAPIDB.RouteSummary.find({});
     if (foundManyDocuments.length > 0) {
       foundManyDocuments.sort((a, b) => (a.route_id > b.route_id ? 1 : -1));
-      await res.status(200).send(foundManyDocuments);
+      return reply.code(200).send(foundManyDocuments);
+      //   return foundManyDocuments;
       console.log('🟢 → Request for "/routes/summary": %s Found', foundManyDocuments.length);
     } else {
-      await res.status(404).send([]);
+      return reply.code(404).send([]);
+      //   return [];
       console.log('🟡 → Request for "/routes/summary": Not Found');
     }
   } catch (err) {
-    await res.status(500).send([]);
+    return reply.code(500).send([]);
     console.log('🔴 → Request for "/routes/summary": Server Error', err);
   }
 });
 
 //
-app.get('/routes/route_id/:route_id', async (req, res) => {
+fastify.get('/routes/route_id/:route_id', async (request, reply) => {
   try {
-    const foundOneDocument = await GTFSAPIDB.Route.findOne({ route_id: req.params.route_id });
+    const foundOneDocument = await GTFSAPIDB.Route.findOne({ route_id: request.params.route_id });
     if (foundOneDocument) {
-      await res.status(200).send(foundOneDocument);
-      console.log('🟢 → Request for "/routes/route_id/%s": 1 Found', req.params.route_id);
+      return reply.code(200).send(foundOneDocument);
+      console.log('🟢 → Request for "/routes/route_id/%s": 1 Found', request.params.route_id);
     } else {
-      await res.status(404).send({});
-      console.log('🟡 → Request for "/routes/route_id/%s": Not Found', req.params.route_id);
+      return reply.code(404).send({});
+      console.log('🟡 → Request for "/routes/route_id/%s": Not Found', request.params.route_id);
     }
   } catch (err) {
-    await res.status(500).send({});
-    console.log('🔴 → Request for "/routes/route_id/%s": Server Error', req.params.route_id, err);
+    return reply.code(500).send({});
+    console.log('🔴 → Request for "/routes/route_id/%s": Server Error', request.params.route_id, err);
   }
 });
 
 //
-app.get('/routes/route_short_name/:route_short_name', async (req, res) => {
+fastify.get('/routes/route_short_name/:route_short_name', async (request, reply) => {
   try {
-    const foundManyDocuments = await GTFSAPIDB.Route.find({ route_id: { $regex: `^${req.params.route_short_name}` } });
+    const foundManyDocuments = await GTFSAPIDB.Route.find({ route_id: { $regex: `^${request.params.route_short_name}` } });
     if (foundManyDocuments.length > 0) {
-      await res.status(200).send(foundManyDocuments);
-      console.log('🟢 → Request for "/routes/route_short_name/%s": %s Found', req.params.route_short_name, foundManyDocuments.length);
+      return reply.code(200).send(foundManyDocuments);
+      console.log('🟢 → Request for "/routes/route_short_name/%s": %s Found', request.params.route_short_name, foundManyDocuments.length);
     } else {
-      await res.status(404).send([]);
-      console.log('🟡 → Request for "/routes/route_short_name/%s": Not Found', req.params.route_short_name);
+      return reply.code(404).send([]);
+      console.log('🟡 → Request for "/routes/route_short_name/%s": Not Found', request.params.route_short_name);
     }
   } catch (err) {
-    await res.status(500).send([]);
-    console.log('🔴 → Request for "/routes/route_short_name/%s": Server Error', req.params.route_short_name, err);
+    return reply.code(500).send([]);
+    console.log('🔴 → Request for "/routes/route_short_name/%s": Server Error', request.params.route_short_name, err);
   }
 });
 
 //
-app.get('/stops', async (req, res) => {
+fastify.get('/stops', async (request, reply) => {
   try {
     const foundManyDocuments = await GTFSAPIDB.Stop.find({});
     if (foundManyDocuments.length > 0) {
       foundManyDocuments.sort((a, b) => (a.stop_id > b.stop_id ? 1 : -1));
-      await res.status(200).send(foundManyDocuments);
+      return reply.code(200).send(foundManyDocuments);
       console.log('🟢 → Request for "/stops/[all]": %s Found', foundManyDocuments.length);
     } else {
-      await res.status(404).send([]);
+      return reply.code(404).send([]);
       console.log('🟡 → Request for "/stops/[all]": Not Found');
     }
   } catch (err) {
-    await res.status(500).send([]);
+    return reply.code(500).send([]);
     console.log('🔴 → Request for "/stops/[all]": Server Error', err);
   }
 });
 
 //
-app.get('/stops/:stop_id', async (req, res) => {
+fastify.get('/stops/:stop_id', async (request, reply) => {
   try {
-    const foundOneDocument = await GTFSAPIDB.Stop.findOne({ stop_id: req.params.stop_id });
+    const foundOneDocument = await GTFSAPIDB.Stop.findOne({ stop_id: request.params.stop_id });
     if (foundOneDocument) {
-      await res.status(200).send(foundOneDocument);
-      console.log('🟢 → Request for "/stops/%s": 1 Found', req.params.stop_id);
+      return reply.code(200).send(foundOneDocument);
+      console.log('🟢 → Request for "/stops/%s": 1 Found', request.params.stop_id);
     } else {
-      await res.status(404).send({});
-      console.log('🟡 → Request for "/stops/%s": Not Found', req.params.stop_id);
+      return reply.code(404).send({});
+      console.log('🟡 → Request for "/stops/%s": Not Found', request.params.stop_id);
     }
   } catch (err) {
-    await res.status(500).send({});
-    console.log('🔴 → Request for "/stops/%s": Server Error', req.params.stop_id, err);
-  }
-});
-
-//
-app.get('/pdf/:stop_id/:route_short_name/:direction_id', async (req, res) => {
-  return await res.status(200).send();
-  try {
-    const pdf_base_url = 'https://raw.githubusercontent.com/carrismetropolitana/pdfs/latest/horarios/';
-    const pdf_filename = `horario-singular-${req.params.stop_id}-${req.params.route_short_name}-${req.params.direction_id}.pdf`;
-    const response = await fetch(pdf_base_url + pdf_filename);
-    if (response.ok) {
-      await res.setHeader('Content-Type', 'application/pdf');
-      await res.setHeader('Content-Disposition', `attachment; filename="${pdf_filename}"`);
-      const pdfStream = Readable.from(response.body);
-      pdfStream.pipe(res);
-      console.log(`🟢 → Request for "/api/pdf/${req.params.stop_id}/${req.params.route_short_name}/${req.params.direction_id}": File Exists`);
-    } else {
-      await res.status(200).send();
-      console.log(`🟡 → Request for "/api/pdf/${req.params.stop_id}/${req.params.route_short_name}/${req.params.direction_id}": File Not Found`);
-    }
-  } catch (err) {
-    await res.status(200).send();
-    console.log(`🔴 → Request for "/api/pdf/${req.params.stop_id}/${req.params.route_short_name}/${req.params.direction_id}": Server Error`, err);
+    return reply.code(500).send({});
+    console.log('🔴 → Request for "/stops/%s": Server Error', request.params.stop_id, err);
   }
 });
 
 // set port, listen for requests
+
+await GTFSAPIDB.connect();
+
 const PORT = 5050;
-app.listen(5050, async () => {
-  console.log('GTFS API listening on port %s...', PORT);
-  await GTFSAPIDB.connect();
-  console.log();
+
+fastify.listen({ port: PORT }, (err) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 });
+
+// fastify.listen({ port: PORT }, async (err) => {
+//   if (err) {
+//     fastify.log.error(err);
+//     process.exit(1);
+//   }
+//   console.log('GTFS API listening on port %s...', PORT);
+//   await GTFSAPIDB.connect();
+//   console.log();
+// });
