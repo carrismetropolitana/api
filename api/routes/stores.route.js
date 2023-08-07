59ed355b000af5d8c0e4533f18b9d333a3afc53e
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-08-07 19:29:44 +0100

    Copy found document first

--- api/routes/stores.route.js
+++ api/routes/stores.route.js
@@ -17,28 +17,31 @@ module.exports.single = async (request, reply) => {
   const foundOneDocument = await GTFSAPIDB.Store.findOne({ code: { $eq: request.params.code } });
   // Return early if nothing is found
   if (!foundOneDocument) return reply.send({});
-  // Setup the four default ticket categories
-  foundOneDocument.status = [
-    { category_code: 'A', currently_waiting: 0 },
-    { category_code: 'B', currently_waiting: 0 },
-    { category_code: 'C', currently_waiting: 0 },
-    { category_code: 'D', currently_waiting: 0 },
-  ];
+  // Setup the four default ticket categories on a copy of the found document
+  const storeDocument = {
+    ...foundOneDocument,
+    status: [
+      { category_code: 'A', currently_waiting: 0 },
+      { category_code: 'B', currently_waiting: 0 },
+      { category_code: 'C', currently_waiting: 0 },
+      { category_code: 'D', currently_waiting: 0 },
+    ],
+  };
   // Query IXAPI for the status of the requested store
-  const result = await IXAPI.request({ storeCode: request.params.code, initialDate: getIxDateString(-7200), finalDate: getIxDateString() });
+  const result = await IXAPI.request({ storeCode: storeDocument.code, initialDate: getIxDateString(-7200), finalDate: getIxDateString() });
   // Return early if request result is undefined
-  if (!result?.content?.ticket?.length) return reply.send(foundOneDocument);
+  if (!result?.content?.ticket?.length) return reply.send(storeDocument);
   // Parse the response result to match the desired structure
   for (const obj of result.content.ticket) {
     // Find index of current category object
-    const categoryIndex = foundOneDocument.status.findIndex((item) => item.category_code === obj.categoryCode);
+    const categoryIndex = storeDocument.status.findIndex((item) => item.category_code === obj.categoryCode);
     // If the categoryCode is not yet present in the result array, add it with total 1
-    if (categoryIndex === -1) foundOneDocument.status.push({ category_code: obj.categoryCode, currently_waiting: 1 });
+    if (categoryIndex === -1) storeDocument.status.push({ category_code: obj.categoryCode, currently_waiting: 1 });
     // If the categoryCode is already present, increase the total count by 1
-    else foundOneDocument.status[categoryIndex].currently_waiting += 1;
+    else storeDocument.status[categoryIndex].currently_waiting += 1;
   }
   // Return result to the caller
-  return reply.send(foundOneDocument);
+  return reply.send(storeDocument);
 };
 
 //
