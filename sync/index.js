8418d803762b6ebf1c2c995bc8972fa1179ad495
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-08-09 23:56:53 +0100

    Logs

--- sync/index.js
+++ sync/index.js
@@ -15,12 +15,14 @@ const syncHelpdesksStatus = require('./tasks/syncHelpdesksStatus');
   // Setup task
   let TASK_SYNC_HELPDESKS_STATUS = false;
   // Schedule task (helper: https://crontab.guru/#*_*_*_*_*)
-  crontab.schedule('* * * * *', async () => {
+  crontab.schedule('* * * * * *', async () => {
     // CHECK IF TASK IS NOT ALREADY RUNNING
     if (!TASK_SYNC_HELPDESKS_STATUS) {
+      console.log('will run task');
       TASK_SYNC_HELPDESKS_STATUS = true;
       await syncHelpdesksStatus();
       TASK_SYNC_HELPDESKS_STATUS = false;
+      console.log('did run task - finish');
     }
   });
 
