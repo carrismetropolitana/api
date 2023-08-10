84286b75c1b42fd9d648a64a906d0d1e977c930c
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-08-10 12:08:39 +0100

    Move crons to the task itself

--- sync/index.js
+++ sync/index.js
@@ -1,9 +1,7 @@
 /* * */
 /* IMPORTS */
-const crontab = require('node-cron');
 const SERVERDB = require('./services/SERVERDB');
 const syncHelpdesksStatus = require('./tasks/syncHelpdesksStatus');
-const syncStopsStatus = require('./tasks/syncStopsStatus');
 
 //
 
@@ -13,33 +11,8 @@ const syncStopsStatus = require('./tasks/syncStopsStatus');
   //
   await SERVERDB.connect();
 
-  // Setup task 1
-  let TASK_SYNC_HELPDESKS_STATUS = false;
-  // Schedule task (helper: https://crontab.guru/#*_*_*_*_*)
-  crontab.schedule('* * * * *', async () => {
-    // CHECK IF TASK IS NOT ALREADY RUNNING
-    if (!TASK_SYNC_HELPDESKS_STATUS) {
-      console.log('-- will run task');
-      TASK_SYNC_HELPDESKS_STATUS = true;
-      await syncHelpdesksStatus();
-      TASK_SYNC_HELPDESKS_STATUS = false;
-      console.log('-- did run task - finish');
-    }
-  });
-
-  //   // Setup task 2
-  //   let TASK_SYNC_STOPS_STATUS = false;
-  //   // Schedule task (helper: https://crontab.guru/#*_*_*_*_*)
-  //   crontab.schedule('* * * * *', async () => {
-  //     // CHECK IF TASK IS NOT ALREADY RUNNING
-  //     if (!TASK_SYNC_STOPS_STATUS) {
-  //       console.log('-- will run task');
-  //       TASK_SYNC_STOPS_STATUS = true;
-  //       await syncStopsStatus();
-  //       TASK_SYNC_STOPS_STATUS = false;
-  //       console.log('-- did run task - finish');
-  //     }
-  //   });
+  // Setup tasks
+  await syncHelpdesksStatus();
 
   //
 })();
--- sync/tasks/syncHelpdesksStatus.js
+++ sync/tasks/syncHelpdesksStatus.js
@@ -1,5 +1,6 @@
 /* * */
 /* IMPORTS */
+const crontab = require('node-cron');
 const SERVERDB = require('../services/SERVERDB');
 const IXAPI = require('../services/IXAPI');
 
@@ -9,29 +10,44 @@ const IXAPI = require('../services/IXAPI');
  * Save each result in MongoDB.
  */
 module.exports = async () => {
-  // Retrieve helpdesks from database
-  const foundManyDocuments = await SERVERDB.Helpdesk.find();
-  // Query IXAPI for the status of the requested helpdesk
-  const allHelpdesksTicketsWaiting = await IXAPI.request({ reportType: 'ticket', status: 'W', initialDate: getIxDateString(-7200), finalDate: getIxDateString() });
-  // Query IXAPI for the status of the requested helpdesk
-  const allHelpdesksStatistics = await IXAPI.request({ reportType: 'entityReport', initialDate: getIxDateString(-7200), finalDate: getIxDateString() });
-  // Add realtime status to each helpdesk
-  for (const foundDocument of foundManyDocuments) {
-    // Filter all waiting ticket by the current helpdesk code
-    const helpdeskTicketsWaiting = allHelpdesksTicketsWaiting?.content?.ticket?.filter((item) => item.siteEID === foundDocument.code);
-    // Find the entityReport entry for the current helpdesk
-    const helpdeskStatistics = allHelpdesksStatistics?.content?.entityReport?.find((item) => item.siteEID === foundDocument.code);
-    // Format the update query with the request results
-    const updatedDocumentValues = {
-      currently_waiting: helpdeskTicketsWaiting?.length || 0, // parseInt(Math.random() * -100),
-      expected_wait_time: helpdeskStatistics?.averageWaitTime || 0, // parseInt(Math.random() * -100),
-    };
-    // Update the current document with the new values
-    await SERVERDB.Helpdesk.findOneAndUpdate({ code: foundDocument.code }, updatedDocumentValues, { new: true, upsert: true });
-    // Log progress
-    console.log(`⤷ Updated Helpdesk ${foundDocument.name} (${foundDocument.code}): currently_waiting: ${updatedDocumentValues.currently_waiting}; expected_wait_time: ${updatedDocumentValues.expected_wait_time}`);
+  // Setup flag to avoid overlapping runs
+  let IS_THIS_TASK_RUNNING = false;
+  // Schedule task (https://crontab.guru/#*_*_*_*_*)
+  crontab.schedule('* * * * *', async () => {
+    // Check if task is already running
+    if (!IS_THIS_TASK_RUNNING) {
+      // Switch the flag ON
+      IS_THIS_TASK_RUNNING = true;
+      // Retrieve helpdesks from database
+      const foundManyDocuments = await SERVERDB.Helpdesk.find();
+      // Query IXAPI for the status of the requested helpdesk
+      const allHelpdesksTicketsWaiting = await IXAPI.request({ reportType: 'ticket', status: 'W', initialDate: getIxDateString(-7200), finalDate: getIxDateString() });
+      // Query IXAPI for the status of the requested helpdesk
+      const allHelpdesksStatistics = await IXAPI.request({ reportType: 'entityReport', initialDate: getIxDateString(-7200), finalDate: getIxDateString() });
+      // Add realtime status to each helpdesk
+      for (const foundDocument of foundManyDocuments) {
+        // Filter all waiting ticket by the current helpdesk code
+        const helpdeskTicketsWaiting = allHelpdesksTicketsWaiting?.content?.ticket?.filter((item) => item.siteEID === foundDocument.code);
+        // Find the entityReport entry for the current helpdesk
+        const helpdeskStatistics = allHelpdesksStatistics?.content?.entityReport?.find((item) => item.siteEID === foundDocument.code);
+        // Format the update query with the request results
+        const updatedDocumentValues = {
+          currently_waiting: helpdeskTicketsWaiting?.length || 0, // parseInt(Math.random() * -100),
+          expected_wait_time: helpdeskStatistics?.averageWaitTime || 0, // parseInt(Math.random() * -100),
+        };
+        // Update the current document with the new values
+        await SERVERDB.Helpdesk.findOneAndUpdate({ code: foundDocument.code }, updatedDocumentValues, { new: true, upsert: true });
+        // Log progress
+        console.log(`⤷ Updated Helpdesk ${foundDocument.name} (${foundDocument.code}): currently_waiting: ${updatedDocumentValues.currently_waiting}; expected_wait_time: ${updatedDocumentValues.expected_wait_time}`);
+        //
+      }
+      // Switch the flag OFF
+      IS_THIS_TASK_RUNNING = false;
+      //
+    }
     //
-  }
+  });
+
   //
 };
 
