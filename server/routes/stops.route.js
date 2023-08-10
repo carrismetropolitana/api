f4b4120687e4025983d098ccede54321827069a8
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-08-10 01:34:40 +0100

    Test

--- server/routes/stops.route.js
+++ server/routes/stops.route.js
@@ -19,12 +19,15 @@ module.exports.single = async (request, reply) => {
 
 //
 module.exports.singleWithRealtime = async (request, reply) => {
+  const foundManyDocuments = await SERVERDB.Stop.find({}, 'code').lean();
+  const allStopCodes = foundManyDocuments.map((item) => item.code);
   const result = await PCGIAPI.request('openservices/estimatedStopSchedules', {
     method: 'POST',
     body: {
       operators: ['41', '42', '43', '44'],
-      stops: [request.params.code],
-      numResults: 15,
+      //   stops: [request.params.code],
+      stops: [allStopCodes],
+      numResults: 1,
     },
   });
   result.forEach((element) => delete element.observedDriverId); // Remove useless property
