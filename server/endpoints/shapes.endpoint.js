cc3f6516064a71eb46756b9e88fda2ec39b0c447
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-08-23 20:19:29 +0100

    Enable all patterns and all shapes endpoints

--- server/endpoints/shapes.endpoint.js
+++ server/endpoints/shapes.endpoint.js
@@ -5,7 +5,12 @@ const SERVERDB = require('../services/SERVERDB');
 //
 module.exports.all = async (request, reply) => {
   // Disabled endpoint
-  return reply.send([]);
+  // return reply.send([]);
+  //
+  const foundManyDocuments = await SERVERDB.Shape.find().lean();
+  const collator = new Intl.Collator('en', { numeric: true, sensitivity: 'base' });
+  foundManyDocuments.sort((a, b) => collator.compare(a.code, b.code));
+  return reply.send(foundManyDocuments || []);
 };
 
 //
