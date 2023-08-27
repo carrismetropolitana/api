3841ce8ebad22d8d70e48d23903a7e9c1dfff890
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-08-27 21:28:46 +0100

    Updated key names

--- server/endpoints/vehicles.endpoint.js
+++ server/endpoints/vehicles.endpoint.js
@@ -8,13 +8,18 @@ module.exports.all = async (request, reply) => {
   const allVehiclesData = await allVehiclesResponse.json();
   const allVehiclesFormatted = allVehiclesData.map((element) => {
     return {
+      code: element.Vid,
+      trip_code: element.Lna,
+      lat: element.Lat,
+      lon: element.Lng,
+      heading: element.Coa,
+      speed: element.Spd,
+      timestamp: element.Ts,
+      // deprecated
       vehicle_id: element.Vid,
       trip_id: element.Lna,
       latitude: element.Lat,
       longitude: element.Lng,
-      heading: element.Coa,
-      speed: element.Spd,
-      timestamp: element.Ts,
     };
   });
   return reply.send(allVehiclesFormatted || []);
