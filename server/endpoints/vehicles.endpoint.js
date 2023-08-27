b86483381c9678be7bd9db8039b68059c84886a9
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-08-27 01:16:12 +0100

    Update vehicles.endpoint.js

--- server/endpoints/vehicles.endpoint.js
+++ server/endpoints/vehicles.endpoint.js
@@ -1,11 +1,12 @@
 /* * */
 /* IMPORTS */
-const PCGIAPI = require('../services/PCGIAPI');
+const { PCGI_BASE_URL_DEV } = process.env;
 
 //
 module.exports.all = async (request, reply) => {
-  const allVehicles = await PCGIAPI.request('vehiclelocation/vehiclePosition/mapVehicles');
-  const allVehiclesFormatted = allVehicles.map((element) => {
+  const allVehiclesResponse = await fetch(`${PCGI_BASE_URL_DEV}/vehiclelocation/vehiclePosition/mapVehicles`);
+  const allVehiclesData = await allVehiclesResponse.json();
+  const allVehiclesFormatted = allVehiclesData.map((element) => {
     return {
       vehicle_id: element.Vid,
       trip_id: element.Lna,
