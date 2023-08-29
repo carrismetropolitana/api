4c7964c93d486763478c277f7be83b4ae21396bc
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-08-29 20:01:09 +0100

    Added pattern_code

--- server/endpoints/vehicles.endpoint.js
+++ server/endpoints/vehicles.endpoint.js
@@ -6,20 +6,21 @@ const { PCGI_BASE_URL_DEV } = process.env;
 module.exports.all = async (request, reply) => {
   const allVehiclesResponse = await fetch(`${PCGI_BASE_URL_DEV}/vehiclelocation/vehiclePosition/mapVehicles`);
   const allVehiclesData = await allVehiclesResponse.json();
-  const allVehiclesFormatted = allVehiclesData.map((element) => {
+  const allVehiclesFormatted = allVehiclesData.map((vehicle) => {
     return {
-      code: element.Vid,
-      trip_code: element.Lna,
-      lat: element.Lat,
-      lon: element.Lng,
-      heading: element.Coa,
-      speed: element.Spd,
-      timestamp: element.Ts,
+      code: vehicle.Vid,
+      lat: vehicle.Lat,
+      lon: vehicle.Lng,
+      speed: vehicle.Spd,
+      timestamp: vehicle.Ts,
+      heading: vehicle.Coa,
+      trip_code: vehicle.Lna,
+      pattern_code: vehicle.Lna?.substring(0, 8),
       // deprecated
-      vehicle_id: element.Vid,
-      trip_id: element.Lna,
-      latitude: element.Lat,
-      longitude: element.Lng,
+      vehicle_id: vehicle.Vid,
+      trip_id: vehicle.Lna,
+      latitude: vehicle.Lat,
+      longitude: vehicle.Lng,
     };
   });
   return reply.send(allVehiclesFormatted || []);
