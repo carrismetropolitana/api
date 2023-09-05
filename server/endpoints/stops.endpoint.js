799755e9e6a11ee7e90153d6e7ccbe92d5616673
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-09-05 01:40:12 +0100

    Fix

--- server/endpoints/stops.endpoint.js
+++ server/endpoints/stops.endpoint.js
@@ -3,6 +3,8 @@
 const SERVERDB = require('../services/SERVERDB');
 const PCGIAPI = require('../services/PCGIAPI');
 
+const regexPattern = /^\d{6}$/; // String with exactly 6 numeric digits
+
 //
 module.exports.all = async (request, reply) => {
   const foundManyDocuments = await SERVERDB.Stop.find().lean();
@@ -13,14 +15,14 @@ module.exports.all = async (request, reply) => {
 
 //
 module.exports.single = async (request, reply) => {
+  if (!regexPattern.test(request.params.code)) return reply.status(400).send([]);
   const foundOneDocument = await SERVERDB.Stop.findOne({ code: { $eq: request.params.code } }).lean();
   return reply.send(foundOneDocument || {});
 };
 
 //
 module.exports.singleWithRealtime = async (request, reply) => {
-  // Test if a variable is a string with exactly 6 numeric digits
-  if (!request.params.code.test(/^\d{6}$/)) return reply.status(400).send([]);
+  if (!regexPattern.test(request.params.code)) return reply.status(400).send([]);
   const result = await PCGIAPI.request(`opcoremanager/stop-schedules/${request.params.code}`);
   result.forEach((element) => delete element.observedDriverId); // Remove useless property
   return reply.send(result || []);
