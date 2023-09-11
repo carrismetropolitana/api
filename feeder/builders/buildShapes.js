314ce6f8c506355f3e8bbe6cbf7d79eb9b986604
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-09-12 00:41:46 +0100

    Handler malformed shapes

--- feeder/builders/buildShapes.js
+++ feeder/builders/buildShapes.js
@@ -32,23 +32,27 @@ module.exports = async () => {
   let updatedShapeCodes = [];
   // Loop through each object in each chunk
   for (const shape of allShapes.rows) {
-    // Initiate a variable to hold the parsed shape
-    let parsedShape = {
-      code: shape.shape_id,
-    };
-    // Sort points to match sequence
-    const collator = new Intl.Collator('en', { numeric: true, sensitivity: 'base' });
-    parsedShape.points = shape.points.sort((a, b) => collator.compare(a.shape_pt_sequence, b.shape_pt_sequence));
-    // Create geojson feature using turf
-    parsedShape.geojson = turf.lineString(parsedShape.points.map((point) => [parseFloat(point.shape_pt_lon), parseFloat(point.shape_pt_lat)]));
-    // Calculate shape extension
-    const shapeExtensionKm = turf.length(parsedShape.geojson, { units: 'kilometers' });
-    const shapeExtensionMeters = shapeExtensionKm ? shapeExtensionKm * 1000 : 0;
-    parsedShape.extension = parseInt(shapeExtensionMeters);
-    // Update or create new document
-    await SERVERDB.Shape.replaceOne({ code: parsedShape.code }, parsedShape, { upsert: true });
-    // Return _id to main thread
-    updatedShapeCodes.push(parsedShape.code);
+    try {
+      // Initiate a variable to hold the parsed shape
+      let parsedShape = {
+        code: shape.shape_id,
+      };
+      // Sort points to match sequence
+      const collator = new Intl.Collator('en', { numeric: true, sensitivity: 'base' });
+      parsedShape.points = shape.points.sort((a, b) => collator.compare(a.shape_pt_sequence, b.shape_pt_sequence));
+      // Create geojson feature using turf
+      parsedShape.geojson = turf.lineString(parsedShape.points.map((point) => [parseFloat(point.shape_pt_lon), parseFloat(point.shape_pt_lat)]));
+      // Calculate shape extension
+      const shapeExtensionKm = turf.length(parsedShape.geojson, { units: 'kilometers' });
+      const shapeExtensionMeters = shapeExtensionKm ? shapeExtensionKm * 1000 : 0;
+      parsedShape.extension = parseInt(shapeExtensionMeters);
+      // Update or create new document
+      await SERVERDB.Shape.replaceOne({ code: parsedShape.code }, parsedShape, { upsert: true });
+      // Return _id to main thread
+      updatedShapeCodes.push(parsedShape.code);
+    } catch (error) {
+      console.log('ERROR parsing shape', shape.code, error);
+    }
   }
   // Delete all Shapes not present in the current update
   const deletedStaleShapes = await SERVERDB.Shape.deleteMany({ code: { $nin: updatedShapeCodes } });
