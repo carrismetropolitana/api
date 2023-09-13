8cca8374d715680e234eb59df88c40abcb449bec
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-09-14 00:14:58 +0100

    All time strings come in 25+

--- server/endpoints/stops.endpoint.js
+++ server/endpoints/stops.endpoint.js
@@ -29,11 +29,22 @@ module.exports.singleWithRealtime = async (request, reply) => {
       trip_code: estimate.tripId,
       headsign: estimate.tripHeadsign,
       stop_sequence: estimate.stopSequence,
-      scheduled_arrival: estimate.stopScheduledArrivalTime || estimate.stopScheduledDepartureTime,
-      estimated_arrival: estimate.stopArrivalEta || estimate.stopDepartureEta,
-      observed_arrival: estimate.stopObservedArrivalTime || estimate.stopObservedDepartureTime,
+      scheduled_arrival: convertTimeStringTo25Hours(estimate.stopScheduledArrivalTime) || convertTimeStringTo25Hours(estimate.stopScheduledDepartureTime),
+      estimated_arrival: convertTimeStringTo25Hours(estimate.stopArrivalEta) || convertTimeStringTo25Hours(estimate.stopDepartureEta),
+      observed_arrival: convertTimeStringTo25Hours(estimate.stopObservedArrivalTime) || convertTimeStringTo25Hours(estimate.stopObservedDepartureTime),
       vehicle_code: estimate.observedVehicleId,
     };
   });
   return reply.send(result || []);
 };
+
+function convertTimeStringTo25Hours(timeString) {
+  const hoursString = timeString.substring(0, 1);
+  const hoursInt = parseInt(hoursString);
+  if (hoursInt < 4) {
+    const hoursInt25 = hoursInt + 24;
+    const hoursString25 = hoursInt25.padStart(2, '0');
+    return `${hoursString25}${timeString.substring(2)}`;
+  }
+  return timeString;
+}
