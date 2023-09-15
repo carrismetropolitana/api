7e0939950c723935e0ddf4784a0d5d931d7d50df
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-09-15 14:47:12 +0100

    Revert

--- sync/services/IXAPI.js
+++ sync/services/IXAPI.js
@@ -43,7 +43,7 @@ class IXAPI {
         Authorization: `Bearer ${this.access_token}`,
         StatisticsFilter: JSON.stringify({
           header: { method: 'statisticsGetReport' },
-          content: { apiVersion: '1.14', rowsPerPage: 2000, ...options },
+          content: { apiVersion: '1.0', rowsPerPage: 2000, ...options },
         }),
       },
       body: options.body ? JSON.stringify(options.body) : undefined,
