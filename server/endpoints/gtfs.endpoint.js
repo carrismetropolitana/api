52803fadcd0f7f1698d89147f1e8dc95201b9588
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-09-23 02:42:04 +0100

    Update gtfs.endpoint.js

--- server/endpoints/gtfs.endpoint.js
+++ server/endpoints/gtfs.endpoint.js
@@ -1,9 +1,13 @@
 //
+import { Buffer } from 'node:buffer';
+
+//
 module.exports.feed = async (request, reply) => {
   const gtfsFeedResponse = await fetch('https://github.com/carrismetropolitana/gtfs/raw/live/CarrisMetropolitana.zip');
-  //   const gtfsFeed = await gtfsFeedResponse.blob();
+  const gtfsFeed = await gtfsFeedResponse.arrayBuffer();
   reply.header('Content-Type', 'application/zip');
   reply.header('Content-Disposition', 'attachment; filename="CarrisMetropolitana.zip"');
   //   return reply.send(gtfsFeed);
-  gtfsFeedResponse.body.pipeTo(reply.raw);
+  const buffer = Buffer.from(gtfsFeed, 'utf-8');
+  reply.send(buffer);
 };
