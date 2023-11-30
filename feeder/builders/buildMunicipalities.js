ba1664e978e5fc6278136519eeae5a5293fce13f
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-11-30 12:07:45 +0000

    Cleanup

--- feeder/builders/buildMunicipalities.js
+++ feeder/builders/buildMunicipalities.js
@@ -1,20 +1,32 @@
+/* * */
+
 const FEEDERDB = require('../services/FEEDERDB');
 const SERVERDB = require('../services/SERVERDB');
 const timeCalc = require('../modules/timeCalc');
 
-/* UPDATE MUNICIPALITIES */
+/* * */
 
 module.exports = async () => {
+  //
+  // 1.
   // Record the start time to later calculate operation duration
   const startTime = process.hrtime();
+
+  // 2.
   // Fetch all Municipalities from Postgres
   console.log(`⤷ Querying database...`);
   const allMunicipalities = await FEEDERDB.connection.query('SELECT * FROM municipalities');
+
+  // 3.
   // Initate a temporary variable to hold updated Municipalities
   const allMunicipalitiesData = [];
   const updatedMunicipalityKeys = new Set();
+
+  // 4.
   // Log progress
   console.log(`⤷ Updating Municipalities...`);
+
+  // 5.
   // For each municipality, update its entry in the database
   for (const municipality of allMunicipalities.rows) {
     // Parse municipality
@@ -31,15 +43,20 @@ module.exports = async () => {
     allMunicipalitiesData.push(parsedMunicipality);
     await SERVERDB.client.set(`municipalities:${parsedMunicipality.id}`, JSON.stringify(parsedMunicipality));
     updatedMunicipalityKeys.add(`municipalities:${parsedMunicipality.id}`);
-    //
   }
+
+  // 6.
   // Log count of updated Municipalities
   console.log(`⤷ Updated ${updatedMunicipalityKeys.size} Municipalities.`);
+
+  // 7.
   // Add the 'all' option
   const collator = new Intl.Collator('en', { numeric: true, sensitivity: 'base' });
   allMunicipalitiesData.sort((a, b) => collator.compare(a.id, b.id));
   await SERVERDB.client.set('municipalities:all', JSON.stringify(allMunicipalitiesData));
   updatedMunicipalityKeys.add('municipalities:all');
+
+  // 8.
   // Delete all Municipalities not present in the current update
   const allSavedMunicipalityKeys = [];
   for await (const key of SERVERDB.client.scanIterator({ TYPE: 'string', MATCH: 'municipalities:*' })) {
@@ -48,8 +65,11 @@ module.exports = async () => {
   const staleMunicipalityKeys = allSavedMunicipalityKeys.filter((id) => !updatedMunicipalityKeys.has(id));
   if (staleMunicipalityKeys.length) await SERVERDB.client.del(staleMunicipalityKeys);
   console.log(`⤷ Deleted ${staleMunicipalityKeys.length} stale Municipalities.`);
+
+  // 9.
   // Log elapsed time in the current operation
   const elapsedTime = timeCalc.getElapsedTime(startTime);
   console.log(`⤷ Done updating Municipalities (${elapsedTime}).`);
+
   //
 };
