f6686c746cfeabe7f7e3d37469d62e5f76c5616e
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-12-02 15:21:07 +0000

    Cleanup

--- server/services/RTEVENTS.js
+++ server/services/RTEVENTS.js
@@ -19,45 +19,37 @@ class RTEVENTS {
 
   async update() {
     //
-
-    //
-    // 1. Skip if last update happened in the past 20 seconds
-
+    // 1.
+    // Skip if last update happened in the past 20 seconds
     if (this.last_update > DateTime.now().minus({ seconds: 20 }).toUnixInteger()) return;
 
-    //
-    // 2. Prepare the request to PCGI to get all events received in the last 5 minutes
-
+    // 2.
+    // Prepare the request to PCGI to get all events received in the last 5 minutes
     const formData = new URLSearchParams();
     formData.append('timeRange', DateTime.now().setZone('Europe/Lisbon').minus({ minutes: 5 }).toFormat('yyyyLLddHHmm'));
 
-    //
-    // 3. Perform the request to PCGI
-
+    // 3.
+    // Perform the request to PCGI
     const allRtEvents = await PCGIAPI.request('opcoreconsole/vehicle-events/filtered', {
       method: 'POST',
       contentType: 'application/x-www-form-urlencoded',
       body: formData,
     });
 
-    //
-    // 4. Set the current time to the last_update flag to avoid over fetching
-
+    // 4.
+    // Set the current time to the last_update flag to avoid over fetching
     this.last_update = DateTime.now().toUnixInteger();
 
-    //
-    // 5. Reset the Map variable
-
+    // 5.
+    // Reset the Map variable
     const updatedRtEvents = new Map();
 
-    //
-    // 6. Update vehicles with the latest events
-
+    // 6.
+    // Update vehicles with the latest events
     for (const rtEvent of allRtEvents) {
       //
-
-      //
-      // 6.1. Perform basic event validations
+      // 6.1.
+      // Perform basic event validations
 
       // Does this event has a valid vehicle id
       if (!rtEvent?.content?.entity[0]?.vehicle?.vehicle?.id?.length) continue;
@@ -70,25 +62,21 @@ class RTEVENTS {
       // Is this event older than 90 seconds
       if (rtEvent?.content?.entity[0]?.vehicle?.timestamp > DateTime.now().minus({ seconds: 90 }).toUnixInteger()) continue;
 
-      //
-      // 6.2. Prepare the most used variables
-
+      // 6.2.
+      // Prepare the most used variables
       const vehicleId = `${rtEvent.content.entity[0].vehicle.agencyId}|${rtEvent.content.entity[0].vehicle.vehicle.id}`;
       const vehicleTimestamp = rtEvent.content.entity[0].vehicle.timestamp;
       const vehicleTripId = rtEvent.content.entity[0].vehicle.trip.tripId;
       const vehicleBearing = Math.floor(rtEvent?.content?.entity[0]?.vehicle?.position?.bearing || 0);
       const vehicleSpeed = rtEvent?.content?.entity[0]?.vehicle?.position?.speed / 3.6 || 0; // in meters per second
 
-      //
-      // 6.3. Check if there is a vehicle already saved and that it has an older timestamp than the current event
-
+      // 6.3.
+      // Check if there is a vehicle already saved and that it has an older timestamp than the current event
       if (this.rt_events[vehicleId] && this.rt_events[vehicleId].timestamp >= vehicleTimestamp) continue;
 
-      //
-      // 6.4. Save the current event
-
+      // 6.4.
+      // Save the current event
       updatedRtEvents.set(vehicleId, {
-        //
         // The vehicle ID is composed of the agency_id and the vehicle_id
         vehicle_id: vehicleId,
         // Event ID should be kept stable for the duration of a single trip
@@ -115,9 +103,8 @@ class RTEVENTS {
       //
     }
 
-    //
-    // 7. Save the updated Map to memory
-
+    // 7.
+    // Save the updated Map to memory
     this.rt_events = updatedRtEvents;
 
     //
