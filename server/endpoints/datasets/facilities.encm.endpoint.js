960f2856f5bdc8bddb9c880f2b09d175ff9235f8
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-01-09 15:29:43 +0000

    New endpoint

--- server/endpoints/datasets/facilities.encm.endpoint.js
+++ server/endpoints/datasets/facilities.encm.endpoint.js
@@ -5,7 +5,7 @@ const SERVERDB = require('../../services/SERVERDB');
 /* * */
 
 module.exports.all = async (request, reply) => {
-  const allItems = await SERVERDB.client.get('encm:all');
+  const allItems = await SERVERDB.client.get('datasets/facilities/encm/all');
   return reply
     .code(200)
     .header('Content-Type', 'application/json; charset=utf-8')
@@ -13,17 +13,9 @@ module.exports.all = async (request, reply) => {
 };
 
 module.exports.single = async (request, reply) => {
-  const singleItem = await SERVERDB.client.get(`encm:${request.params.id}`);
+  const singleItem = await SERVERDB.client.get(`datasets/facilities/encm/${request.params.id}`);
   return reply
     .code(200)
     .header('Content-Type', 'application/json; charset=utf-8')
     .send(singleItem || {});
-};
-
-module.exports.test = async (request, reply) => {
-  const allItems = await SERVERDB.client.get('datasets/facilities/encm/all');
-  return reply
-    .code(200)
-    .header('Content-Type', 'application/json; charset=utf-8')
-    .send(allItems || []);
 };
