0aada3f3e877365b9102a1cf6075dbd7f6b4d443
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-02-10 03:22:12 +0000

    Cleanup

--- server/endpoints/network/vehicles.endpoint.js
+++ server/endpoints/network/vehicles.endpoint.js
@@ -1,45 +1,12 @@
 /* * */
 
-const { DateTime } = require('luxon');
-const PCGIAPI = require('../../services/PCGIAPI');
 const protobuf = require('protobufjs');
 const RTEVENTS = require('../../services/RTEVENTS');
 const gtfsRealtime = protobuf.loadSync(`${process.env.PWD}/services/gtfs-realtime.proto`);
 
 /* * */
 
-function convertToUTC(localUnixTimestampMili) {
-  // Create a Date object with the local Unix timestamp and local timezone
-  return DateTime.fromMillis(localUnixTimestampMili, { zone: 'UTC' }).setZone('Europe/Lisbon', { keepLocalTime: true }).toUTC().toUnixInteger();
-}
-
-/* * */
-
 module.exports.json = async (request, reply) => {
-  // Fetch all vehicles
-  //   const allVehiclesData = await PCGIAPI.request('vehiclelocation/vehiclePosition/mapVehicles');
-  // Keep only vehicles that match conditions
-  //   const allVehiclesFiltered = allVehiclesData.filter((vehicle) => {
-  //     return true;
-  //     const vehicleIsInTrip = vehicle.Ss === 'STARTED' || vehicle.Ss === 'RUNNING';
-  //     return vehicleIsInTrip;
-  //   });
-  // Map to API format
-  //   const allVehiclesFormatted = allVehiclesFiltered.map((vehicle) => {
-  //     return {
-  //       id: vehicle.Vid,
-  //       lat: vehicle.Lat,
-  //       lon: vehicle.Lng,
-  //       speed: vehicle.Spd,
-  //       status: vehicle.Ss,
-  //       timestamp: convertToUTC(vehicle.Ts),
-  //       heading: vehicle.Coa,
-  //       trip_id: vehicle.Lna,
-  //       line_id: vehicle.Lna?.substring(0, 4),
-  //       route_id: vehicle.Lna?.substring(0, 6),
-  //       pattern_id: vehicle.Lna?.substring(0, 8),
-  //     };
-  //   });
   // Get the saved events from RTEVENTS
   const rtFeed = await RTEVENTS.json();
   // Return response
