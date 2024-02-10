f1af68904a3a0adab5ef15f1479b1f2cd9c28923
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-02-10 03:20:58 +0000

    Simplified GTFS-RT management

--- server/services/RTEVENTS.js
+++ server/services/RTEVENTS.js
@@ -1,7 +1,6 @@
 /* * */
 
 const { DateTime } = require('luxon');
-const PCGIAPI = require('./PCGIAPI');
 const REALTIMEDB = require('./REALTIMEDB');
 
 /* * */
@@ -18,20 +17,14 @@ class RTEVENTS {
 
   async update() {
     //
+
     // 1.
     // Skip if last update happened in the past 20 seconds
+
     if (this.last_update > DateTime.now().minus({ seconds: 20 }).toUnixInteger()) return;
 
     // 2.
-    // Prepare the request to PCGI to get all events received in the last 5 minutes
-    const formData = new URLSearchParams();
-    formData.append('limit', 50000);
-    formData.append('operatorId', '');
-    formData.append('vehicleId', '');
-    formData.append('timeRange', DateTime.now().setZone('Europe/Lisbon').minus({ minutes: 5 }).toFormat('yyyyLLddHHmm'));
-
-    // 3.
-    // Perform the request to PCGI
+    // Fetch latest events
 
     const allRtEvents = await REALTIMEDB.VehicleEvents.find({
       millis: {
@@ -42,40 +35,23 @@ class RTEVENTS {
       .limit(50000)
       .toArray();
 
-    const allRtEvents2 = await PCGIAPI.request('opcoreconsole/vehicle-events/filtered', {
-      method: 'POST',
-      contentType: 'application/x-www-form-urlencoded',
-      body: formData,
-    });
-
-    // console.log('---------------------------------------------------------------------------------');
-    // console.log('---------------------------------------------------------------------------------');
-    // console.log('---------------------------------------------------------------------------------');
-    // console.log('EVENTS LENGTH');
-    // console.log('---------------------------------------------------------------------------------');
-    // console.log('allRtEvents', JSON.stringify(allRtEvents, null, 2));
-    // console.log('---------------------------------------------------------------------------------');
-    // console.log('allRtEvents2', JSON.stringify(allRtEvents2, null, 2));
-    // console.log('---------------------------------------------------------------------------------');
-    // console.log('---------------------------------------------------------------------------------');
-    // console.log('---------------------------------------------------------------------------------');
-
-    // 4.
+    // 3.
     // Set the current time to the last_update flag to avoid over fetching
+
     this.last_update = DateTime.now().toUnixInteger();
 
-    // 5.
+    // 4.
     // Reset the Map variable
+
     const updatedRtEvents = new Map();
 
-    // SORT EVENTS BY TIMESTAMP ASCENDING
-    // allRtEvents.sort((a, b) => a.content?.entity[0]?.vehicle?.timestamp - b.content?.entity[0]?.vehicle?.timestamp);
-
-    // 6.
+    // 5.
     // Update vehicles with the latest events
+
     for (const rtEvent of allRtEvents) {
       //
-      // 6.1.
+
+      // 5.1.
       // Perform basic event validations
 
       // Does this event has a valid vehicle id
@@ -95,20 +71,23 @@ class RTEVENTS {
       // Is this event older than 90 seconds
       if (rtEvent?.content?.entity[0]?.vehicle?.timestamp < DateTime.now().minus({ seconds: 90 }).toUnixInteger()) continue;
 
-      // 6.2.
+      // 5.2.
       // Prepare the most used variables
+
       const vehicleId = `${rtEvent.content.entity[0].vehicle.agencyId}|${rtEvent.content.entity[0].vehicle.vehicle._id}`;
       const vehicleTimestamp = rtEvent.content.entity[0].vehicle.timestamp;
       const vehicleTripId = rtEvent.content.entity[0].vehicle.trip.tripId;
       const vehicleBearing = Math.floor(rtEvent?.content?.entity[0]?.vehicle?.position?.bearing || 0);
       const vehicleSpeed = rtEvent?.content?.entity[0]?.vehicle?.position?.speed / 3.6 || 0; // in meters per second
 
-      // 6.3.
+      // 5.3.
       // Check if there is a vehicle already saved and that it has an older timestamp than the current event
+
       if (this.rt_events[vehicleId] && this.rt_events[vehicleId].timestamp >= vehicleTimestamp) continue;
 
-      // 6.4.
+      // 5.4.
       // Save the current event
+
       updatedRtEvents.set(vehicleId, {
         // The vehicle ID is composed of the agency_id and the vehicle_id
         vehicle_id: vehicleId,
@@ -116,14 +95,20 @@ class RTEVENTS {
         event_id: `${vehicleId}-${vehicleTripId}`,
         // Timestamp is in UTC
         timestamp: vehicleTimestamp,
-        // Current status can be 'IN_TRANSIT_TO', 'INCOMMING_AT' or 'STOPPED_AT' at the current stop_id
-        current_status: rtEvent.content.entity[0].vehicle.currentStatus,
         // Schedule relationship can be SCHEDULED for planned trips or ADDED for new trips created by the driver
         schedule_relationship: rtEvent.content.entity[0].vehicle.trip.scheduleRelationship === 'SCHEDULED' ? 'SCHEDULED' : 'DUPLICATED',
-        // Trip ID, route ID and stop ID should always be a known entity in the scheduled GTFS
+        // Trip ID, Pattern ID, Route ID and Line ID should always be known entities in the scheduled GTFS
         trip_id: vehicleTripId,
+        pattern_id: rtEvent.content.entity[0].vehicle.trip.patternId,
         route_id: rtEvent.content.entity[0].vehicle.trip.routeId,
+        line_id: rtEvent.content.entity[0].vehicle.trip.lineId,
+        // The stop the vehicle is serving at the moment
         stop_id: rtEvent.content.entity[0].vehicle.stopId,
+        // Current status can be 'IN_TRANSIT_TO', 'INCOMMING_AT' or 'STOPPED_AT' at the current stop_id
+        current_status: rtEvent.content.entity[0].vehicle.currentStatus,
+        //
+        block_id: rtEvent.content.entity[0].vehicle.vehicle.blockId,
+        shift_id: rtEvent.content.entity[0].vehicle.vehicle.shiftId,
         //
         latitude: rtEvent.content.entity[0].vehicle.position.latitude,
         longitude: rtEvent.content.entity[0].vehicle.position.longitude,
@@ -136,8 +121,9 @@ class RTEVENTS {
       //
     }
 
-    // 7.
+    // 6.
     // Save the updated Map to memory
+
     this.rt_events = updatedRtEvents;
 
     //
@@ -150,13 +136,13 @@ class RTEVENTS {
   async protobuf() {
     //
 
-    //
-    // 1. Request an update of the events
+    // 1.
+    // Request an update of the events
 
     await this.update();
 
-    //
-    // 2. Prepare the feed header
+    // 2.
+    // Prepare the feed header
 
     const feed = {
       header: {
@@ -167,8 +153,8 @@ class RTEVENTS {
       entity: [],
     };
 
-    //
-    // 3. Prepare the entities array
+    // 3.
+    // Prepare the entities array
 
     feed.entity = Array.from(this.rt_events.values()).map((savedEvent) => ({
       id: savedEvent.event_id,
@@ -193,8 +179,8 @@ class RTEVENTS {
       },
     }));
 
-    //
-    // 4. Return the feed to the caller
+    // 4.
+    // Return the feed to the caller
 
     return feed;
 
@@ -208,27 +194,42 @@ class RTEVENTS {
   async json() {
     //
 
-    //
-    // 1. Request an update of the events
+    // 1.
+    // Request an update of the events
 
     await this.update();
 
-    //
-    // 2. Return the formatted result
+    // 2.
+    // Return the formatted result
 
     return Array.from(this.rt_events.values()).map((savedEvent) => ({
+      //
       id: savedEvent.vehicle_id,
+      //
+      timestamp: savedEvent.timestamp,
+      //
+      schedule_relationship: savedEvent.schedule_relationship,
+      //
+      trip_id: savedEvent.trip_id,
+      pattern_id: savedEvent.pattern_id,
+      route_id: savedEvent.route_id,
+      line_id: savedEvent.line_id,
+      //
+      stop_id: savedEvent.stop_id,
+      current_status: savedEvent.current_status,
+      //
+      block_id: savedEvent.block_id,
+      shift_id: savedEvent.shift_id,
+      //
       lat: savedEvent.latitude,
       lon: savedEvent.longitude,
-      speed: savedEvent.speed,
-      bearing: savedEvent.bearing,
-      timestamp: savedEvent.timestamp,
-      trip_id: savedEvent.trip_id,
-      pattern_id: savedEvent.trip_id?.substring(0, 8),
-      stop_id: savedEvent.stop_id,
       //
-      status: 'DEPRECATED PROPERTY', // deprecated
+      bearing: savedEvent.bearing,
+      speed: savedEvent.speed,
+      ////
+      status: 'REPLACED BY current_status', // deprecated
       heading: 0, // deprecated
+      //
     }));
 
     //
