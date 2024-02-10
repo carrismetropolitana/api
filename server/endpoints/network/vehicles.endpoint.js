241963857c8f683b663558e973af6c550f5720fa
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-02-10 02:13:37 +0000

    Use events for vehicle positions

--- server/endpoints/network/vehicles.endpoint.js
+++ server/endpoints/network/vehicles.endpoint.js
@@ -17,34 +17,36 @@ function convertToUTC(localUnixTimestampMili) {
 
 module.exports.json = async (request, reply) => {
   // Fetch all vehicles
-  const allVehiclesData = await PCGIAPI.request('vehiclelocation/vehiclePosition/mapVehicles');
+  //   const allVehiclesData = await PCGIAPI.request('vehiclelocation/vehiclePosition/mapVehicles');
   // Keep only vehicles that match conditions
-  const allVehiclesFiltered = allVehiclesData.filter((vehicle) => {
-    return true;
-    const vehicleIsInTrip = vehicle.Ss === 'STARTED' || vehicle.Ss === 'RUNNING';
-    return vehicleIsInTrip;
-  });
+  //   const allVehiclesFiltered = allVehiclesData.filter((vehicle) => {
+  //     return true;
+  //     const vehicleIsInTrip = vehicle.Ss === 'STARTED' || vehicle.Ss === 'RUNNING';
+  //     return vehicleIsInTrip;
+  //   });
   // Map to API format
-  const allVehiclesFormatted = allVehiclesFiltered.map((vehicle) => {
-    return {
-      id: vehicle.Vid,
-      lat: vehicle.Lat,
-      lon: vehicle.Lng,
-      speed: vehicle.Spd,
-      status: vehicle.Ss,
-      timestamp: convertToUTC(vehicle.Ts),
-      heading: vehicle.Coa,
-      trip_id: vehicle.Lna,
-      line_id: vehicle.Lna?.substring(0, 4),
-      route_id: vehicle.Lna?.substring(0, 6),
-      pattern_id: vehicle.Lna?.substring(0, 8),
-    };
-  });
+  //   const allVehiclesFormatted = allVehiclesFiltered.map((vehicle) => {
+  //     return {
+  //       id: vehicle.Vid,
+  //       lat: vehicle.Lat,
+  //       lon: vehicle.Lng,
+  //       speed: vehicle.Spd,
+  //       status: vehicle.Ss,
+  //       timestamp: convertToUTC(vehicle.Ts),
+  //       heading: vehicle.Coa,
+  //       trip_id: vehicle.Lna,
+  //       line_id: vehicle.Lna?.substring(0, 4),
+  //       route_id: vehicle.Lna?.substring(0, 6),
+  //       pattern_id: vehicle.Lna?.substring(0, 8),
+  //     };
+  //   });
+  // Get the saved events from RTEVENTS
+  const rtFeed = await RTEVENTS.json();
   // Return response
   return reply
     .code(200)
     .header('Content-Type', 'application/json; charset=utf-8')
-    .send(allVehiclesFormatted || []);
+    .send(rtFeed || []);
 };
 
 /* * */
