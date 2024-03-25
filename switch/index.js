3eddad86da4d5040b56f4b647790e8510e5570ed
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-03-25 11:59:39 +0000

    Do horarios QR Code handler

--- switch/index.js
+++ switch/index.js
@@ -5,6 +5,7 @@ const fastify = require('fastify')({ logger: true, requestTimeout: 10000 });
 /* * */
 
 fastify.get('/pip/:id', require('./handlers/pip.handler').handler);
+fastify.get('/horarios/:line_id/:stop_id', require('./handlers/horarios.handler').handler);
 
 /* * */
 
