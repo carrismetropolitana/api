947347f77335e6ddef859725115bc481cbe23231
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-04-22 18:07:02 +0100

    Updated horarios handler

--- switch/handlers/horarios.handler.js
+++ switch/handlers/horarios.handler.js
@@ -1,5 +1,5 @@
 /* * */
 
 module.exports.handler = async (request, reply) => {
-  return reply.redirect(`https://on.carrismetropolitana.pt/lines/${request.params.line_id}`);
+	return reply.redirect(`https://on.carrismetropolitana.pt/lines/${request.params.line_id}`);
 };
--- switch/index.js
+++ switch/index.js
@@ -5,11 +5,11 @@ const fastify = require('fastify')({ logger: true, requestTimeout: 10000 });
 /* * */
 
 fastify.get('/pip/:id', require('./handlers/pip.handler').handler);
-fastify.get('/horarios/:line_id/:stop_id', require('./handlers/horarios.handler').handler);
+fastify.get('/horarios/:line_id/:direction_id/:stop_id', require('./handlers/horarios.handler').handler);
 
 /* * */
 
 fastify.listen({ port: 5050, host: '0.0.0.0' }, async (err, address) => {
-  if (err) throw err;
-  console.log(`Server listening on ${address}`);
+	if (err) throw err;
+	console.log(`Server listening on ${address}`);
 });
