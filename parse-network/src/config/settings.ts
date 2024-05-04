773441aa086583856b1d539fc3b67d15cea93813
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-05-04 01:32:48 +0100

    Whitespace changes

--- parse-network/src/config/settings.ts
+++ parse-network/src/config/settings.ts
@@ -1,30 +1,30 @@
 /* * */
 
-export const RUN_INTERVAL = 300000; // 5 minutes
+export const RUN_INTERVAL = 300000 // 5 minutes
 
-export const SINGLE_RUN = false || process.env.SINGLE_RUN === 'true';
+export const SINGLE_RUN = false || process.env.SINGLE_RUN === 'true'
 
 /* * */
 
 export const ENABLED_MODULES = [
-	'gtfs_import',
-	'municipalities_parser',
-	'localities_parser',
-	'periods_parser',
-	'dates_parser',
-	'plans_parser',
-	'stops_parser',
-	'shapes_parser',
-	'lines_routes_patterns_parser',
-	'timetables_parser',
-];
+  'gtfs_import',
+  // 'municipalities_parser',
+  // 'localities_parser',
+  // 'periods_parser',
+  // 'dates_parser',
+  // 'plans_parser',
+  'stops_parser',
+  // 'shapes_parser',
+  'lines_routes_patterns_parser',
+  // 'timetables_parser',
+]
 
 /* * */
 
-export const BASE_DIR = '/tmp/base';
-export const GTFS_BASE_DIR = 'gtfs';
-export const GTFS_RAW_DIR = 'raw';
-export const GTFS_PREPARED_DIR = 'prepared';
-export const NETEX_BASE_DIR = 'netex';
-export const NETEX_RAW_DIR = 'raw';
-export const NETEX_PREPARED_DIR = 'prepared';
+export const BASE_DIR = '/tmp/base'
+export const GTFS_BASE_DIR = 'gtfs'
+export const GTFS_RAW_DIR = 'raw'
+export const GTFS_PREPARED_DIR = 'prepared'
+export const NETEX_BASE_DIR = 'netex'
+export const NETEX_RAW_DIR = 'raw'
+export const NETEX_PREPARED_DIR = 'prepared'
