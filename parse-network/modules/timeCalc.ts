9507a52ec15f7bd877f94ada122b6578abef6385
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-05-05 19:02:54 +0100

    Remove bigint literal

--- parse-network/modules/timeCalc.ts
+++ parse-network/modules/timeCalc.ts
@@ -1,51 +1,51 @@
 /* * */
 
 export function getElapsedTime(startTime: [number, number]) {
-  const interval = process.hrtime(startTime);
+	const interval = process.hrtime(startTime);
 
-  const elapsedMiliseconds = Math.floor(
-    // seconds -> milliseconds +
-    interval[0] * 1000
+	const elapsedMiliseconds = Math.floor(
+		// seconds -> milliseconds +
+		interval[0] * 1000 +
     // + nanoseconds -> milliseconds
-    + interval[1] / 1000000,
-  );
+    interval[1] / 1000000,
+	);
 
-  //
-  const dateObj = new Date(elapsedMiliseconds);
-  //
-  const milliseconds = dateObj.getMilliseconds();
-  const seconds = dateObj.getSeconds();
-  const minutes = dateObj.getMinutes();
-  const hours = dateObj.getHours();
+	//
+	const dateObj = new Date(elapsedMiliseconds);
+	//
+	const milliseconds = dateObj.getMilliseconds();
+	const seconds = dateObj.getSeconds();
+	const minutes = dateObj.getMinutes();
+	const hours = dateObj.getHours();
 
-  let string = '';
+	let string = '';
 
-  if (hours > 0) { string += `${hours}h `; }
-  if (minutes > 0) { string += `${minutes}m `; }
-  if (seconds > 0) { string += `${seconds}s `; }
-  if (milliseconds > 0) { string += `${milliseconds}ms`; }
+	if (hours > 0) { string += `${hours}h `; }
+	if (minutes > 0) { string += `${minutes}m `; }
+	if (seconds > 0) { string += `${seconds}s `; }
+	if (milliseconds > 0) { string += `${milliseconds}ms`; }
 
-  return string;
-  //
+	return string;
+	//
 }
 
 export function formatTime(time: bigint) {
-  // time is in ns
+// time is in ns
 
-  const dateObj = new Date(Number(time / 1000000n));
-  //
-  const milliseconds = dateObj.getMilliseconds();
-  const seconds = dateObj.getSeconds();
-  const minutes = dateObj.getMinutes();
-  const hours = dateObj.getHours();
+	const dateObj = new Date(Number(time / BigInt(1000000)));
+	//
+	const milliseconds = dateObj.getMilliseconds();
+	const seconds = dateObj.getSeconds();
+	const minutes = dateObj.getMinutes();
+	const hours = dateObj.getHours();
 
-  let string = '';
+	let string = '';
 
-  if (hours > 0) { string += `${hours}h `; }
-  if (minutes > 0) { string += `${minutes}m `; }
-  if (seconds > 0) { string += `${seconds}s `; }
-  if (milliseconds > 0) { string += `${milliseconds}ms`; }
+	if (hours > 0) { string += `${hours}h `; }
+	if (minutes > 0) { string += `${minutes}m `; }
+	if (seconds > 0) { string += `${seconds}s `; }
+	if (milliseconds > 0) { string += `${milliseconds}ms`; }
 
-  return string;
-  //
+	return string;
+//
 }
