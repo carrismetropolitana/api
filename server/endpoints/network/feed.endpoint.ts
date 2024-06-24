aa45e09dcd6fae9e962a2ef934606c43d575024b
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-06-24 16:39:23 +0100

    Try to fix GTFS redirect

--- server/endpoints/network/feed.endpoint.ts
+++ server/endpoints/network/feed.endpoint.ts
@@ -1,18 +1,22 @@
 /* * */
 
-import { Buffer } from 'buffer';
+// import { Buffer } from 'buffer';
 
 /* * */
 
 const gtfs = async (_, reply) => {
 	const gtfsFeedResponse = await fetch(process.env.GTFS_URL);
-	const gtfsFeed = await gtfsFeedResponse.arrayBuffer();
-	const gtfsFeedBbuffer = Buffer.from(new Uint8Array(gtfsFeed).toString(), 'utf-8');
+
+	// const gtfsFeedResponse = await fetch(process.env.GTFS_URL);
+	// const gtfsFeed = await gtfsFeedResponse.arrayBuffer();
+
+	// const gtfsFeed = await gtfsFeedResponse.arrayBuffer();
+	// const gtfsFeedBbuffer = Buffer.from(new Uint8Array(gtfsFeed).toString(), 'utf-8');
 	return reply
 		.code(200)
 		.header('Content-Type', 'application/zip')
 		.header('Content-Disposition', 'attachment; filename="CarrisMetropolitana.zip"')
-		.send(gtfsFeedBbuffer || null);
+		.send(gtfsFeedResponse || null);
 };
 
 /* * */
