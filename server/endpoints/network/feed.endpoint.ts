a10f9ced39367221bd6d30c527747858fbfd7263
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-06-24 16:44:31 +0100

    Cleanup

--- server/endpoints/network/feed.endpoint.ts
+++ server/endpoints/network/feed.endpoint.ts
@@ -1,17 +1,7 @@
 /* * */
 
-// import { Buffer } from 'buffer';
-
-/* * */
-
 const gtfs = async (_, reply) => {
 	const gtfsFeedResponse = await fetch(process.env.GTFS_URL);
-
-	// const gtfsFeedResponse = await fetch(process.env.GTFS_URL);
-	// const gtfsFeed = await gtfsFeedResponse.arrayBuffer();
-
-	// const gtfsFeed = await gtfsFeedResponse.arrayBuffer();
-	// const gtfsFeedBbuffer = Buffer.from(new Uint8Array(gtfsFeed).toString(), 'utf-8');
 	return reply
 		.code(200)
 		.header('Content-Type', 'application/zip')
