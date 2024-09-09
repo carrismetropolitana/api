5f1d99fd03f10c8696641a35b41eb842fbb01a7f
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-09-09 22:03:22 +0100

    Update parseAlertV2.ts

--- sync-alerts/services/parseAlertV2.ts
+++ sync-alerts/services/parseAlertV2.ts
@@ -4,12 +4,17 @@ export default function parseAlertV2(item) {
 	//
 
 	const parsedInformedEntity = item.alert.informedEntity.map((entity) => {
+		console.log('-----------------------------------------------');
+		console.log('entity', entity);
+		console.log('---');
 		if (entity.routeId) {
+			console.log('entity.routeId', entity.routeId);
 			return {
 				lineId: entity.routeId.substring(0, 4),
 				...entity,
 			};
 		}
+		console.log('-----------------------------------------------');
 		return entity;
 	});
 
