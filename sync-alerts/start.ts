fc12874e8fe5eb4465192aa5008e1c960a0a2664
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-09-11 16:31:12 +0100

    Update packages, add send alert notifications functionality

--- sync-alerts/start.ts
+++ sync-alerts/start.ts
@@ -4,6 +4,7 @@ import SERVERDB from '@/services/SERVERDB.js';
 import parseAlertV2 from '@/services/parseAlertV2.js';
 import LOGGER from '@helperkits/logger';
 import TIMETRACKER from '@helperkits/timer';
+import crypto from 'node:crypto';
 
 /* * */
 
@@ -25,16 +26,57 @@ export default async () => {
 	LOGGER.info(`Fetched Alerts feed from the backoffice (${backofficeTimer.get()})`);
 
 	//
-	// Prepare the alerts data in JSON and Protobuf formats
+	// Prepare the alerts data in Protobuf format
 
-	const saveTimer = new TIMETRACKER();
-
-	const allAlertsArray = alertsFeedData?.entity.map(item => parseAlertV2(item));
-	await SERVERDB.client.set(`v2/network/alerts/json`, JSON.stringify(allAlertsArray));
+	const protobufTimer = new TIMETRACKER();
 
 	await SERVERDB.client.set(`v2/network/alerts/protobuf`, JSON.stringify(alertsFeedData));
 
-	LOGGER.info(`Saved ${allAlertsArray.length} Alerts to ServerDB (${saveTimer.get()})`);
+	LOGGER.info(`Saved Protobuf Alerts to ServerDB (${protobufTimer.get()})`);
+
+	//
+	// Prepare the alerts data in JSON format
+
+	const jsonTimer = new TIMETRACKER();
+
+	const allAlertsParsedV2 = alertsFeedData?.entity.map(item => parseAlertV2(item));
+
+	await SERVERDB.client.set(`v2/network/alerts/json`, JSON.stringify(allAlertsParsedV2));
+
+	LOGGER.info(`Saved ${allAlertsParsedV2.length} JSON Alerts to ServerDB (${jsonTimer.get()})`);
+
+	//
+	// Send notifications for new alerts
+
+	const notificationsTimer = new TIMETRACKER();
+
+	const allSentNotificationsTxt = await SERVERDB.client.get(`v2/network/alerts/sent_notifications`);
+	const allSentNotifications = await JSON.parse(allSentNotificationsTxt);
+
+	const hashFunction = crypto.createHash('sha1');
+	const allAlertsParsedV2Hashes = allAlertsParsedV2.map(alertData => ({
+		alert: alertData,
+		hash: hashFunction.update(JSON.stringify(alertData)).digest('hex'),
+	}));
+
+	// Send the notifications
+
+	let sentNotificationCounter = 0;
+
+	for (const alertItem of allAlertsParsedV2Hashes) {
+		if (!allSentNotifications.includes(alertItem.hash)) {
+			// sendNotification(alertItem.alert);
+			sentNotificationCounter++;
+			allSentNotifications.push(alertItem.hash);
+			LOGGER.success(`Sent notification for alert: ${alertItem.alert.alert_id}`);
+		}
+	}
+
+	await SERVERDB.client.set(`v2/network/alerts/sent_notifications`, JSON.stringify(allSentNotifications));
+
+	LOGGER.info(`Sent ${sentNotificationCounter} Notifications (${notificationsTimer.get()})`);
+
+	//
 
 	LOGGER.terminate(`Done with this iteration (${globalTimer.get()})`);
 
