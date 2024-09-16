4e8163a482b4102265ff6caf012fa21d36d74f1a
Jusi Monteiro <jusi.monteiro@protonmail.com>
2024-09-16 17:41:05 +0100

    Refactor demand endpoint to add route parameters for line and stop IDs

--- server/endpoints/v2/metrics/demand.endpoint.ts
+++ server/endpoints/v2/metrics/demand.endpoint.ts
@@ -21,6 +21,25 @@ const byLine = async (_, reply) => {
 		.send(allItems || []);
 };
 
+const byLineId = async (request, reply) => {
+	const allItems = await SERVERDB.client.get(`v2/metrics/demand/by_line`);
+	
+	const lineId = request.params.lineId;
+	const line = allItems.find((item) => item.lineId === lineId);
+	
+	if (!line) {
+		return reply
+			.code(404)
+			.header('Content-Type', 'application/json; charset=utf-8')
+			.send({ message: 'Not found' });
+	}
+
+	return reply
+		.code(200)
+		.header('Content-Type', 'application/json; charset=utf-8')
+		.send(line);
+}
+
 const byStop = async (_, reply) => {
 	const allItems = await SERVERDB.client.get('v2/metrics/demand/by_stop');
 	return reply
@@ -29,12 +48,35 @@ const byStop = async (_, reply) => {
 		.send(allItems || []);
 };
 
+const byStopId = async (request, reply) => {
+	const allItems = await SERVERDB.client.get(`v2/metrics/demand/by_stop`);
+	
+	const stopId = request.params.stopId;
+	const stop = allItems.find((item) => item.stopId === stopId);
+	
+	if (!stop) {
+		return reply
+			.code(404)
+			.header('Content-Type', 'application/json; charset=utf-8')
+			.send({ message: 'Not found' });
+	}
+
+	return reply
+		.code(200)
+		.header('Content-Type', 'application/json; charset=utf-8')
+		.send(stop);
+}
+
 /* * */
 
 FASTIFY.server.get('/metrics/demand/by_day', byDay);
 FASTIFY.server.get('/metrics/demand/by_line', byLine);
+FASTIFY.server.get('/metrics/demand/by_line/:lineId', byLineId);
 FASTIFY.server.get('/metrics/demand/by_stop', byStop);
+FASTIFY.server.get('/metrics/demand/by_stop/:stopId', byStopId);
 
 FASTIFY.server.get('/v2/metrics/demand/by_day', byDay);
 FASTIFY.server.get('/v2/metrics/demand/by_line', byLine);
+FASTIFY.server.get('/v2/metrics/demand/by_line/:lineId', byLineId);
 FASTIFY.server.get('/v2/metrics/demand/by_stop', byStop);
+FASTIFY.server.get('/v2/metrics/demand/by_stop/:stopId', byStopId);
