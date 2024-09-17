499d5478800836a5daabf15b5adac69348d21888
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-09-17 12:59:11 +0100

    Remove comments

--- server/endpoints/v2/network/feed.endpoint.ts
+++ server/endpoints/v2/network/feed.endpoint.ts
@@ -6,12 +6,6 @@ import FASTIFY from '@/services/FASTIFY.js';
 
 const main = async (_, reply) => {
 	reply.code(302).redirect(new URL(process.env.GTFS_URL).href);
-	// const gtfsFeedResponse = await fetch(process.env.GTFS_URL);
-	// return reply
-	// 	.code(200)
-	// 	.header('Content-Type', 'application/zip')
-	// 	.header('Content-Disposition', 'attachment; filename="CarrisMetropolitana.zip"')
-	// 	.send(gtfsFeedResponse || null);
 };
 
 /* * */
