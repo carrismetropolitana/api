dc719ae0ec52b2b6fc52ce2668ce3759c7a48c80
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-09-20 12:52:49 +0100

    temp

--- server/endpoints/v2/network/patterns.endpoint.ts
+++ server/endpoints/v2/network/patterns.endpoint.ts
@@ -44,10 +44,6 @@ const realtime = async (request, reply) => {
 		.map(async (item) => {
 			const route = await SERVERDB.client.get(`v2/network/routes/${item.routeId}`);
 
-			console.log('\n\n ==================== \n\n');
-			console.log('item', item);
-			console.log('\n\n ==================== \n\n');
-
 			return {
 				estimated_arrival: item.stopArrivalEta || item.stopDepartureEta,
 				estimated_arrival_unix: DATES.convert24HourPlusOperationTimeStringToUnixTimestamp(item.stopArrivalEta) || DATES.convert24HourPlusOperationTimeStringToUnixTimestamp(item.stopDepartureEta),
@@ -56,7 +52,7 @@ const realtime = async (request, reply) => {
 				observed_arrival: item.stopObservedArrivalTime || item.stopObservedDepartureTime,
 				observed_arrival_unix: DATES.convert24HourPlusOperationTimeStringToUnixTimestamp(item.stopObservedArrivalTime) || DATES.convert24HourPlusOperationTimeStringToUnixTimestamp(item.stopObservedDepartureTime),
 				pattern_id: item.patternId,
-				route: route && JSON.parse(route),
+				route: route || 'na',
 				route_id: item.routeId,
 				scheduled_arrival: item.stopScheduledArrivalTime || item.stopScheduledDepartureTime,
 				scheduled_arrival_unix: DATES.convert24HourPlusOperationTimeStringToUnixTimestamp(item.stopScheduledArrivalTime) || DATES.convert24HourPlusOperationTimeStringToUnixTimestamp(item.stopScheduledDepartureTime),
