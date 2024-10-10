1b840456b78d60cf2131c21156fa70806bc6b999
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-10-10 19:47:47 +0100

    Refactor endpoint parameter names for consistency

--- server/endpoints/v2/metrics/sla.endpoint.ts
+++ server/endpoints/v2/metrics/sla.endpoint.ts
@@ -6,12 +6,12 @@ import SERVERDB from '@/services/SERVERDB.js';
 /* * */
 
 const byLine = async (request, reply) => {
-	const lineId = request.params.line_id;
-	const operationalDay = request.params.operational_day || 'all';
+	const lineId = request.params.lineId;
+	const operationalDay = request.params.operationalDay || 'all';
 
-	const metric = await SERVERDB.client.get(`v2:metrics:sla:${lineId}:${operationalDay}`);
+	const metrics = await SERVERDB.client.get(`v2:metrics:sla:${lineId}:${operationalDay}`);
 
-	if (!metric) {
+	if (!metrics) {
 		return reply
 			.code(404)
 			.header('Content-Type', 'application/json; charset=utf-8')
@@ -21,10 +21,10 @@ const byLine = async (request, reply) => {
 	return reply
 		.code(200)
 		.header('Content-Type', 'application/json; charset=utf-8')
-		.send(metric);
+		.send(metrics);
 };
 
-const all = async (_, reply) => {
+const all = async (request, reply) => {
 	const metric = await SERVERDB.client.get(`v2:metrics:sla:all`);
 	return reply
 		.code(200)
@@ -34,5 +34,6 @@ const all = async (_, reply) => {
 
 /* * */
 
-FASTIFY.server.get('/v2/metrics/sla/by_line/:line_id/:operational_day', byLine);
+FASTIFY.server.get('/v2/metrics/sla/byLine/:lineId', byLine);
+FASTIFY.server.get('/v2/metrics/sla/byLine/:lineId/:operationalDay', byLine);
 FASTIFY.server.get('/v2/metrics/sla/all', all);
