a62f43f037019e2bfd8d26bf6b5c4806726dba59
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-10-24 11:42:19 +0100

    Test new icon

--- apps/server/src/endpoints/pips/estimates.endpoint.ts
+++ apps/server/src/endpoints/pips/estimates.endpoint.ts
@@ -146,7 +146,7 @@ FASTIFY.server.post<RequestSchema>('/pips/estimates', async (request, reply) => 
 			return {
 				estimatedArrivalTime: compensatedEstimatedArrival,
 				estimatedDepartureTime: compensatedEstimatedArrival,
-				estimatedTimeString: `${estimatedTimeInMinutes} min`,
+				estimatedTimeString: `⦿ ${estimatedTimeInMinutes} min`,
 				estimatedTimeUnixSeconds: estimatedTimeInUnixSeconds,
 				journeyId: estimate.tripId,
 				lineId: estimate.lineId,
