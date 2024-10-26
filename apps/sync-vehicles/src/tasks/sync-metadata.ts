61ee216f939926faa86b7994a5992ac3008710f4
João Pereira <67809513+joaodcp@users.noreply.github.com>
2024-10-26 01:30:25 +0100

    fix: uniform vehicle license plates

--- apps/sync-vehicles/src/tasks/sync-metadata.ts
+++ apps/sync-vehicles/src/tasks/sync-metadata.ts
@@ -49,7 +49,9 @@ export const syncMetadata = async () => {
 			capacity_total: Number(itemCsv.capacity_seated) + Number(itemCsv.capacity_standing),
 			emission_class: convertEmissionClassCode(itemCsv.emission_class),
 			id: `${itemCsv.agency_id}|${itemCsv.vehicle_id}`,
-			license_plate: itemCsv.license_plate,
+			license_plate: (!itemCsv.license_plate.includes('-') && itemCsv.license_plate.length == 6)
+				? itemCsv.license_plate.replace(/(.{2})(.{2})(.{2})/, "$1-$2-$3")
+				: itemCsv.license_plate,
 			make: itemCsv.make,
 			model: itemCsv.model,
 			owner: itemCsv.owner,
