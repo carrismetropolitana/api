b9458c443ceea9a9493502fec841a49f6cfb30b7
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-11-14 11:39:13 +0000

    ESLint changes

--- packages/services/src/TRINODB.ts
+++ packages/services/src/TRINODB.ts
@@ -1,12 +1,11 @@
-
 /* * */
-import { readFileSync } from 'fs';
-
-import { QueryOptions, TrinoService } from './trino.service.js';
-import { SshConfig, SshTunnelService, SshTunnelServiceOptions } from './ssh-tunnel.service.js';
-import { AddressInfo } from 'net';
 
 import { TrinoValidations } from '@carrismetropolitana/api-types/trino';
+import { readFileSync } from 'fs';
+import { AddressInfo } from 'net';
+
+import { SshConfig, SshTunnelService, SshTunnelServiceOptions } from './ssh-tunnel.service.js';
+import { QueryOptions, TrinoService } from './trino.service.js';
 
 /* * */
 
@@ -38,15 +37,15 @@ const sshOptions: SshTunnelServiceOptions = {
 };
 
 export interface CountValidationsOptions {
-	timeUnit?: 'day' | 'hour' | 'month' | 'year' | 'quarter' | 'week',
-	type?: 'line' | 'stop',
 	options?: Omit<QueryOptions, 'orderBy'>
+	timeUnit?: 'day' | 'hour' | 'month' | 'quarter' | 'week' | 'year'
+	type?: 'line' | 'stop'
 }
 
 export interface CountValidationsResult {
-	count_result: number,
-	item_id?: string,
-	transaction_time: string,
+	count_result: number
+	item_id?: string
+	transaction_time: string
 }
 
 class TRINODBClass {
@@ -70,14 +69,14 @@ class TRINODBClass {
 		try {
 			// Connect to the SSH tunnel
 			await this.sshTunnelService.connect();
-            
+
 			if (this.sshTunnelService.server) {
 				// Connect to the Trino service
 				this.trinoService = new TrinoService({
-					host: `http://${process.env.TRINO_TUNNEL_LOCAL_HOST}:${(this.sshTunnelService.server?.address() as AddressInfo).port}`,
-                    user: process.env.TRINODB_USER || 'undefined',
 					catalog: process.env.TRINODB_CATALOG || 'undefined',
+					host: `http://${process.env.TRINO_TUNNEL_LOCAL_HOST}:${(this.sshTunnelService.server?.address() as AddressInfo).port}`,
 					schema: process.env.TRINODB_SCHEMA || 'undefined',
+					user: process.env.TRINODB_USER || 'undefined',
 				});
 
 				console.log(`⤷ Connected to TRINODB.`);
@@ -89,22 +88,10 @@ class TRINODBClass {
 		}
 	}
 
-	async getValidations(options?: QueryOptions) {
-		return await this.trinoService.findMany<TrinoValidations>('t_stg_validations', options);
-	}
-
-	async countValidations({
-		timeUnit,
-		type,
-		options,
-	}: {
-		timeUnit?: 'day' | 'hour' | 'month' | 'year' | 'quarter' | 'week',
-		type?: 'line' | 'stop',
-		options?: Omit<QueryOptions, 'orderBy'>
-	}): Promise<CountValidationsResult[]> {
+	async countValidations({ options, timeUnit, type }: CountValidationsOptions): Promise<CountValidationsResult[]> {
 		const typeId = type === 'line' ? 'linelongid' : 'stoplongid';
 		const whereClause = this.trinoService.buildWhereClause(options?.where);
-		
+
 		let timeColumn: string;
 		switch (timeUnit) {
 			case 'day':
@@ -116,55 +103,58 @@ class TRINODBClass {
 			case 'month':
 				timeColumn = `DATE_TRUNC('month', CAST(REPLACE(transactiondate, 'T', ' ') AS TIMESTAMP))`;
 				break;
-			case 'year':
-				timeColumn = `DATE_TRUNC('year', CAST(REPLACE(transactiondate, 'T', ' ') AS TIMESTAMP))`;
-				break;
 			case 'quarter':
 				timeColumn = `DATE_TRUNC('quarter', CAST(REPLACE(transactiondate, 'T', ' ') AS TIMESTAMP))`;
 				break;
 			case 'week':
 				timeColumn = `DATE_TRUNC('week', CAST(REPLACE(transactiondate, 'T', ' ') AS TIMESTAMP))`;
 				break;
+			case 'year':
+				timeColumn = `DATE_TRUNC('year', CAST(REPLACE(transactiondate, 'T', ' ') AS TIMESTAMP))`;
+				break;
 			default:
 				throw new Error(`Invalid time unit: ${timeUnit}`);
 		}
-		
+
 		const sql = type ? `
-			SELECT 
-				COUNT(DISTINCT transactionid) AS count_result,
-				${typeId}, 
-				${timeColumn} AS transaction_${timeUnit}
-			FROM 
-				t_stg_validations
-			${whereClause}
-			GROUP BY  ${typeId}, ${timeColumn}
-			ORDER BY transaction_${timeUnit}
-		` : `
-			SELECT 
-				COUNT(DISTINCT transactionid) AS count_result,
-				${timeColumn} AS transaction_${timeUnit}
-			FROM 
-				t_stg_validations
-			${whereClause}
-			GROUP BY ${timeColumn}
-			ORDER BY transaction_${timeUnit}
-		`
-		
+				SELECT
+					COUNT(DISTINCT transactionid) AS count_result,
+					${typeId},
+					${timeColumn} AS transaction_${timeUnit}
+				FROM
+					t_stg_validations
+				${whereClause}
+				GROUP BY  ${typeId}, ${timeColumn}
+				ORDER BY transaction_${timeUnit}
+			` : `
+				SELECT
+					COUNT(DISTINCT transactionid) AS count_result,
+					${timeColumn} AS transaction_${timeUnit}
+				FROM
+					t_stg_validations
+				${whereClause}
+				GROUP BY ${timeColumn}
+				ORDER BY transaction_${timeUnit}
+			`;
+
 		const resultsIterator = await this.trinoService.executeQuery(sql);
 		const headers = type ? ['count_result', typeId, `transaction_${timeUnit}`] : ['count_result', `transaction_${timeUnit}`];
 		const results = await this.trinoService.convertIteratorToObject(headers, resultsIterator);
-		
+
 		return results.map(result => ({
 			count_result: Number(result.count_result),
 			item_id: result[typeId],
-			transaction_time: result[`transaction_${timeUnit}`]
+			transaction_time: result[`transaction_${timeUnit}`],
 		})) as CountValidationsResult[];
 	}
 
-    async rawQuery(sql: string) {
-        return await this.trinoService.executeQuery(sql);
-    }
-}
+	async getValidations(options?: QueryOptions) {
+		return await this.trinoService.findMany<TrinoValidations>('t_stg_validations', options);
+	}
 
+	async rawQuery(sql: string) {
+		return await this.trinoService.executeQuery(sql);
+	}
+}
 
 export const TRINODB = TRINODBClass.getInstance();
