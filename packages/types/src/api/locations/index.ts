b312d94569f253f76bbe4c4ec712e233cd010224
João de Vasconcelos <contact@joao.earth>
2025-01-21 23:13:36 +0000

    Remove regions endpoint and related type definitions

--- packages/types/src/api/locations/index.ts
+++ packages/types/src/api/locations/index.ts
@@ -4,4 +4,3 @@ export * from '@/api/locations/districts.type.js';
 export * from '@/api/locations/localities.type.js';
 export * from '@/api/locations/municipalities.type.js';
 export * from '@/api/locations/parishes.type.js';
-export * from '@/api/locations/regions.type.js';
