d3d127148154be37dcd42cf2d4934ad5a8d569c2
João de Vasconcelos <contact@joao.earth>
2025-01-30 21:46:50 +0000

    Add endpoint for videowall VKM metrics retrieval

--- apps/server/src/endpoints/metrics/metrics.endpoint.ts
+++ apps/server/src/endpoints/metrics/metrics.endpoint.ts
@@ -101,3 +101,12 @@ FASTIFY.GET('/metrics/videowall/validations', async (_, reply) => {
 		.header('cache-control', 'public, max-age=60')
 		.send(allItemsTxt);
 });
+
+FASTIFY.GET('/metrics/videowall/vkm', async (_, reply) => {
+	const allItemsTxt = await SERVERDB.get(SERVERDB_KEYS.METRICS.VIDEOWALL.VKM);
+	if (!allItemsTxt) return reply.code(404).send([]);
+	return reply
+		.code(200)
+		.header('cache-control', 'public, max-age=60')
+		.send(allItemsTxt);
+});
