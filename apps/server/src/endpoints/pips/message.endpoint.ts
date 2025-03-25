dc4e64d69ce50d303741b668eae81bf6e6cd63f3
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-03-25 15:34:13 +0000

    Remove testing indication from message response in PIP endpoint

--- apps/server/src/endpoints/pips/message.endpoint.ts
+++ apps/server/src/endpoints/pips/message.endpoint.ts
@@ -17,5 +17,5 @@ FASTIFY.GET<RequestSchema>('/pips/:pip_id/message', async (request, reply) => {
 	return reply
 		.code(200)
 		.header('cache-control', 'public, max-age=20')
-		.send({ message: `Painel em Testes | PIP ID ${request.params.pip_id} | ${DateTime.local({ zone: 'Europe/Lisbon' }).toFormat('HH:mm:ss')}` });
+		.send({ message: `PIP ID ${request.params.pip_id} | ${DateTime.local({ zone: 'Europe/Lisbon' }).toFormat('HH:mm:ss')}` });
 });
