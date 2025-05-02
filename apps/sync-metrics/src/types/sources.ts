3e87b5920d6618246ce3dae0dc9ef4ea046cee51
Bruno Castelo <brunoalexcastelo@gmail.com>
2025-05-02 16:43:13 +0100

    Fix type

--- apps/sync-metrics/src/types/sources.ts
+++ apps/sync-metrics/src/types/sources.ts
@@ -1,7 +1,7 @@
 export interface ComplaintsMetadataSource {
 	_id: string
 	complaints: number
-	current_date: Date
+	current_date: string
 	email: number
 	filter_value: string
 	info_requests: number
