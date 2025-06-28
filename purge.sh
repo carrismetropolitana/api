21a62f6c804a4bfce4083975f57155b52ccbd3b9
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-06-28 08:37:05 +0100

    Updated packages; new purge script

--- purge.sh
+++ purge.sh
@@ -1,9 +1,48 @@
 #!/bin/sh
 
-echo "Starting purge..."
+echo "→ Starting cleanup..."
+echo ""
 
-find . -type d \( -name "node_modules" -o -name ".next" -o -name "dist" -o -name ".turbo" \) -prune -exec rm -rf {} +
+# # #
 
-find . -type f -name "package-lock.json" -exec rm -f {} +
+echo "→ Removing 'node_modules' directories..."
+find . -type d -name "node_modules" -prune | xargs rm -rf
+echo "✓ Done"
+echo ""
 
-echo "Purge complete!"
+echo "→ Removing 'dist' directories..."
+find . -type d -name "dist" -prune | xargs rm -rf
+echo "✓ Done"
+echo ""
+
+echo "→ Removing '.next' directories..."
+find . -type d -name ".next" -prune | xargs rm -rf
+echo "✓ Done"
+echo ""
+
+echo "→ Removing '.turbo' directories..."
+find . -type d -name ".turbo" -prune | xargs rm -rf
+echo "✓ Done"
+echo ""
+
+echo "→ Removing '.source' directories..."
+find . -type d -name ".source" -prune | xargs rm -rf
+echo "✓ Done"
+echo ""
+
+# # #
+
+echo "→ Removing 'pnpm-lock.yaml' files..."
+find . -type f -name "pnpm-lock.yaml" | xargs rm -f
+echo "✓ Done"
+echo ""
+
+echo "→ Removing 'package-lock.json' files..."
+find . -type f -name "package-lock.json" | xargs rm -f
+echo "✓ Done"
+echo ""
+
+# # #
+
+echo "✓ Cleanup complete!"
+echo ""
