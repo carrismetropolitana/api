457c169150b6fd4d7566de14f8f0954bdd86726e
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-09-21 16:34:06 +0100

    Revert "Save all patterns to the db"
    This reverts commit aa207702a0e213dba478bf9c994c28e83be32eba.

--- apps/sync-gtfs/src/tasks/sync-lines-routes-patterns.ts
+++ apps/sync-gtfs/src/tasks/sync-lines-routes-patterns.ts
@@ -94,8 +94,6 @@ export const syncLinesRoutesPatterns = async () => {
 
 	const allLinesParsed = new Map<string, Line>();
 	const allRoutesParsed = new Map<string, Route>();
-	const allPatternsParsed: Record<string, Pattern[]> = {};
-
 	const updatedPatternKeys = new Set<string>();
 
 	for (const patternId of allDistinctPatternIds) {
@@ -449,8 +447,6 @@ export const syncLinesRoutesPatterns = async () => {
 		await SERVERDB.set(SERVERDB_KEYS.NETWORK.PATTERNS.ID(patternId), JSON.stringify(finalizedPatternGroupsData));
 		updatedPatternKeys.add(SERVERDB_KEYS.NETWORK.PATTERNS.ID(patternId));
 
-		allPatternsParsed[patternId] = finalizedPatternGroupsData;
-
 		// LOGGER.info(`Updated pattern_id "${patternId}" (${intraPatternTimer.get()})`);
 
 		//
@@ -472,12 +468,6 @@ export const syncLinesRoutesPatterns = async () => {
 	}
 
 	LOGGER.info(`Deleted ${stalePatternKeys.length} stale Patterns`);
-
-	//
-	// Save all patterns to the database
-
-	await SERVERDB.set(SERVERDB_KEYS.NETWORK.PATTERNS.BASE, JSON.stringify(allPatternsParsed));
-	LOGGER.info(`Updated ${Object.keys(allPatternsParsed).length} Patterns`);
 
 	//
 	// Save all routes to the database
