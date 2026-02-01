56f9bdd8eef05dc6386054e29d67b37193005009
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-02-01 21:25:11 +0000

    Update the entity

--- apps/sync-alerts/src/services/parseAlertV2.ts
+++ apps/sync-alerts/src/services/parseAlertV2.ts
@@ -15,8 +15,8 @@ export default function parseAlertV2(item): Alert {
 		}
 		if (entity.trip) {
 			return {
-				route_id: entity.trip.trip_id.substring(0, 6),
-				line_id: entity.trip.trip_id.substring(0, 4),
+				line_id: entity.trip.trip_id.substring(6, 10),
+				route_id: entity.trip.trip_id.substring(6, 12),
 				...entity,
 			};
 		}
