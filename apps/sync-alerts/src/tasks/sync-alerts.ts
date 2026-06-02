0f7e77d129bd8dd10f39232c2f4784ce307b87b8
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-06-02 16:23:17 +0100

    fix(sync-alerts): update alerts feed URL to the correct endpoint

--- apps/sync-alerts/src/tasks/sync-alerts.ts
+++ apps/sync-alerts/src/tasks/sync-alerts.ts
@@ -25,7 +25,7 @@ export const syncAlerts = async () => {
 
 	const backofficeTimer = new TIMETRACKER();
 
-	const alertsFeedResponse = await fetchData<ServiceAlertResponse>('https://go.tmlmobilidade.pt/hub/api/v1/alerts/gtfs');
+	const alertsFeedResponse = await fetchData<ServiceAlertResponse>('https://go.tmlmobilidade.pt/hub/api/v1/alerts/gtfs-cm');
 
 	if (alertsFeedResponse.error) {
 		LOGGER.error(`Failed to fetch Alerts feed from the backoffice: ${alertsFeedResponse.error}`);
